893bd42bc01f3f9511dd3b777135f41fba6aacb9
Andrey Dimitrov <andrey.v.dimitrov@gmail.com>
2014-03-05 12:29:51 -0500

    legend position improvement

--- plotly/fig2plotly_aux/extractLegend.m
+++ plotly/fig2plotly_aux/extractLegend.m
@@ -11,11 +11,35 @@ legend = {};
 if strcmp(a.Visible, 'on')
     
     %POSITION
-    legend.x = a.Position(1)+a.Position(3)/2;
-    legend.y = a.Position(2)+a.Position(4)/2;    
-    legend.xanchor = 'middle';
-    legend.yanchor = 'middle';
-  
+    x_ref = a.Position(1)+a.Position(3)/2;
+    y_ref = a.Position(2)+a.Position(4)/2;
+    if x_ref>0.333
+        if x_ref>0.666
+            legend.x = a.Position(1)+a.Position(3);
+            legend.xanchor = 'right';
+        else
+            legend.x = a.Position(1)+a.Position(3)/2;
+            legend.xanchor = 'middle';
+        end
+    else
+        legend.x = a.Position(1);
+        legend.xanchor = 'left';
+    end
+    
+    if y_ref>0.333
+        if y_ref>0.666
+            legend.y = a.Position(2)+a.Position(4);
+            legend.yanchor = 'top';
+        else
+            legend.y = a.Position(2)+a.Position(4)/2;
+            legend.yanchor = 'middle';
+        end
+    else
+        legend.y = a.Position(2);
+        legend.yanchor = 'bottom';
+    end
+    
+    
 end
 
 end
