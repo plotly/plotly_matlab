955d06dd176f9c12d61fa826c93cad8b9afd8b69
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-05-26 13:28:29 -0400

    fixed filed paths for PC

--- plotlysetup.m
+++ plotlysetup.m
@@ -8,27 +8,27 @@ function plotlysetup( username, api_key )
 try %check number of inputs
     if nargin ~= 2
         error('plotly:setupInputs',....
-            ['Whoops! Wrong number of inputs. Please setup your Plotly '...
-            '\n\t\t\tMATLAB API by calling >>plotlysetup(' '''user_name''' ',' '''api_key''' ') \n\n']);
+            ['\n\nWhoops! Wrong number of inputs. Please setup your Plotly '...
+            '\nMATLAB API by calling >>plotlysetup(' '''user_name''' ',' '''api_key''' ') \n\n']);
     end
 catch exception %plotlysetup input problem catch...
-    fprintf(['\n\n' exception.identifier '\t --- \t' exception.message]);
+    fprintf(['\n\n' exception.identifier exception.message]);
     return
 end
 
 try %check to see if plotly is in the searchpath
-    plotlysetupPath = which('plotlysetup'); 
+    plotlysetupPath = which('plotlysetup');
     plotlyFolderPath = [plotlysetupPath(1:end-length('plotlysetup.m')) 'plotly']; %there has to be a nicer way of doing this!
     %if it was not found
     if (strcmp(genpath(plotlyFolderPath),''))
         error('plotly:plotlyFilePath',...
-            ['Shoot! It looks like MATLAB is having trouble finding the current version '  ...
-            '\n\t\t\tof Plotly. Please make sure that the plotly/ API folder is in the same '  ...
-            '\n\t\t\tdirectory as plotlysetup.m. Contact chuck@plot.ly for more information. \n\n']);
+            ['\n\nShoot! It looks like MATLAB is having trouble finding the current version '  ...
+            '\nof Plotly. Please make sure that the plotly/ API folder is in the same '  ...
+            '\ndirectory as plotlysetup.m. Contact chuck@plot.ly for more information. \n\n']);
     end
     addpath(genpath(plotlyFolderPath));
 catch exception %plotly file not found problem catch
-    fprintf(['\n\n' exception.identifier '\t --- \t' exception.message]);
+    fprintf(['\n\n' exception.identifier exception.message]);
     return
 end
 
@@ -37,26 +37,38 @@ if(~is_octave)%if MATLAB
     try %embed the api to the matlabroot/toolbox dir.
         fprintf('\nAdding Plotly to MATLAB toolbox directory ... ');
         
-        %matlab root directory
-        mtlroot = fullfile(matlabroot,'toolbox'); %this should work on all platforms
-        
-        %create the plotly folder in the matlab/toolbox dir.
-        plotlyToolboxPath = [mtlroot '/plotly'];
-        overwrite = 'n'; 
+        %plotly folder in the matlab/toolbox dir.
+        plotlyToolboxPath = fullfile(matlabroot,'toolbox','plotly'); %this should work on all platforms
+        overwrite = 'n';
         
         if(exist(plotlyToolboxPath,'dir')) %check for overwrite...
-            fprintf(['\n\n[UPDATE]: \t\t --- \t Hey! We see that a copy of Plotly has previously been added to\n\t\t\t ' ...
-                'your Matlab toolboxes. Would you like us to overwrite it with:\n\t\t\t ' plotlyFolderPath ' ? \n\t\t\t '...
+            fprintf(['\n\n[UPDATE]: \n\nHey! We see that a copy of Plotly has previously been added to\n' ...
+                'your Matlab toolboxes. Would you like us to overwrite it with:\n' plotlyFolderPath ' ? \n'...
                 'Careful! You may lose data saved to this folder.\n\n']);
             overwrite = input('Overwrite (y/n) ? : ','s');
             if(strcmpi(overwrite,'y'));
-                fprintf(['\n\n[OVERWRITE]: \t\t --- \t Overwriting Plotly! ... Done \n\n']);
+                fprintf(['\n[OVERWRITE]:\n\nOverwriting Plotly! ... Done \n\n']);
             else
-                fprintf(['\n\n[OVERWRITE]: \t\t --- \t Did not overwrite Plotly! ... Done \n\n']);
+                fprintf(['\n[OVERWRITE]:\n\nDid not overwrite Plotly! ... Done \n\n']);
             end
         end
-            
+        
         if(~exist(plotlyToolboxPath,'dir')||strcmpi(overwrite,'y'))
+            
+            if(strcmpi(overwrite,'y'))
+                %remove the older version from matlabs searchpath 
+                rmpath(genpath(plotlyToolboxPath)); 
+                %delete the older version
+                [status, message, messageid] = rmdir(plotlyToolboxPath,'s');
+                %check that the folder was deleted
+                if (status == 0)
+                    error('plotly:deletePlotlyAPI',...
+                        ['\n\nShoot! It looks like something went wrong removing the Plotly API ' ...
+                        'from the MATLAB toolbox directory \n' ...
+                        'Please contact your system admin. or chuck@plot.ly for more information. \n\n']);
+                end
+            end
+            
             
             %make the plotlyToolboxPath dir.
             [status, mess, messid] = mkdir(plotlyToolboxPath);
@@ -65,20 +77,18 @@ if(~is_octave)%if MATLAB
             if (status == 0)
                 if(~strcmp(messid, 'MATLAB:MKDIR:DirectoryExists'))
                     error('plotly:savePlotlyAPI',...
-                        ['Shoot! It looks like you might not have write permission ' ...
-                        'for the MATLAB toolbox directory \n\t\t\t' ...
-                        'Please contact your system admin. or chuck@plot.ly for more information. In\n\t\t\tthe ' ...
-                        'mean time you can add the Plotly API to your search path manually whenever you need it! \n']);
+                        ['\n\nShoot! It looks like you might not have write permission ' ...
+                        'for the MATLAB toolbox directory \n' ...
+                        'Please contact your system admin. or chuck@plot.ly for more information. In the ' ...
+                        'mean \ntime you can add the Plotly API to your search path manually whenever you need it! \n\n']);
                 end
             end
             
-            %move a copy of the plotly api to matlab root directory %should check with user for overwrite...but we're not!
+            %move a copy of the plotly api to matlab root directory (does not remove files simply overwrites common files!)
             [status, mess, messid] = copyfile(plotlyFolderPath,plotlyToolboxPath, 'f');
             %check that the plotly api was copied to the matlab root toolbox directory
             if (status == 0)
-                disp(mess)
-                disp(messid)
-
+                
                 if(~strcmp(messid, 'MATLAB:COPYFILE:SourceAndDestinationSame'))
                     error('plotly:copyPlotlyAPI',...
                         ['Shoot! It looks like you might not have write permission ' ...
@@ -89,8 +99,8 @@ if(~is_octave)%if MATLAB
             end
             
             if(~strcmpi(overwrite,'y'))
-            %worked (without interuption)...just a formatting thing! 
-            fprintf('Done\n');
+                %worked (without interuption)...just a formatting thing!
+                fprintf('Done\n');
             end
             
         end
@@ -103,10 +113,10 @@ if(~is_octave)%if MATLAB
             
             %check for a startup.m file in matlab rootpath (we want to add one here)
             startupFile = [];
-            startupFileRootPath = fullfile(matlabroot,'toolbox','local');
-            if(~exist([startupFileRootPath '/startup.m'],'file'))
-                startFileID = fopen([startupFileRootPath '/startup.m'], 'w');
-                startupFile = {[startupFileRootPath '/startup.m']}; %needed because matlab only looks for startup.m when first opened.
+            startupFileRootPath = fullfile(matlabroot,'toolbox','local','startup.m');
+            if(~exist(startupFileRootPath,'file'))
+                startFileID = fopen(startupFileRootPath, 'w');
+                startupFile = {startupFileRootPath}; %needed because matlab only looks for startup.m when first opened.
                 if(startFileID == -1)
                     error('plotly:startFileCreation',...
                         ['Shoot! It looks like you might not have write permission ' ...
@@ -131,15 +141,15 @@ if(~is_octave)%if MATLAB
             end
             
         catch exception %writing to startup.m permission problem catch...
-            fprintf(['\n\n' exception.identifier '\t --- \t' exception.message]);
+            fprintf(['\n\n' exception.identifier exception.message]);
         end
         
     catch exception %copying to toolbox permission problem catch...
-        fprintf(['\n\n' exception.identifier '\t --- \t' exception.message]);
+        fprintf(['\n\n' exception.identifier exception.message]);
     end
     
 else %if octave
-    display('Octave users: Automatic Plotly API embedding coming soon!');
+    display('\nOctave users: Automatic Plotly API embedding coming soon!\n');
 end %end check for matlab...
 
 try %save user credentials
@@ -150,7 +160,7 @@ try %save user credentials
     fprintf('Done\n\n');
     
 catch exception %writing credentials file permission problem catch...
-    fprintf(['\n\n' exception.identifier '\t --- \t' exception.message]);
+    fprintf(['\n\n' exception.identifier exception.message]);
     signin(username,api_key);
 end
 
