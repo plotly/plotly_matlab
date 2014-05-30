2ea5026f48b7aa0c7670baa549c3e3a8a8cc8ebc
oleg komarov <o.komarov11@imperial.ac.uk>
2014-05-30 14:52:58 +0100

    Added overloaded plot method
    Signed-off-by: oleg komarov <o.komarov11@imperial.ac.uk>

--- oo/plotly.m
+++ oo/plotly.m
@@ -1,17 +1,18 @@
 classdef plotly
-% PLOTLY 
+    % PLOTLY
     %  Summary of this class goes here
     %   Detailed explanation goes here
     
-    properties 
-        User = '';
+    properties
+        User     = '';
+        Response = '';
     end
     
     properties(Access = private)
         Key = '';
     end
     
-    methods 
+    methods
         
         function obj = plotly(user,key)
             % PLOTLY Sign into plot.ly
@@ -28,11 +29,90 @@ classdef plotly
             end
         end
         
-        function obj = signin(obj, user, key)
-            % SIGNIN Sign into plot.ly with username and key
+        function obj = plot(obj,varargin)
+            hf = figure('visible','off');
+            ha = axes('parent',hf);
+            plot(ha,varargin{:})
+            obj = obj.fig2plotly(hf);
+        end
+        
+        function obj = fig2plotly(obj, varargin)
+            % FIG2PLOTLY Converts a Matlab figure object into a Plotly figure
             
-            obj.User = user;
-            obj.Key  = key;
+            %   [response] = fig2plotly()
+            %   [response] = fig2plotly(gcf)
+            %   [response] = fig2plotly(f)
+            %   [response] = fig2plotly(gcf, 'property', value, ...)
+            %   [response] = fig2plotly(f, 'property', value, ...)
+            %       gcf - root figure object in the form of a double.
+            %       f - root figure object in the form of a struct. Use f = get(gcf); to
+            %           get the current figure struct.
+            %       List of valid properties:
+            %           'name' - ('untitled')string, name of the plot
+            %           'strip' - (false)boolean, ignores all styling, uses plotly defaults
+            %           'open' - (true)boolean, opens a browser window with plot result
+            %       response - a struct containing the result info of the plot
+            %
+            % For full documentation and examples, see https://plot.ly/api
+            
+            %default input
+            f = get(gcf);
+            plot_name = 'untitled';
+            strip_style = false;
+            open_browser = true;
+            
+            switch numel(varargin)
+                case 0
+                case 1
+                    if isa(varargin{1}, 'double')
+                        f = get(varargin{1});
+                    end
+                    if isa(varargin{1}, 'struct')
+                        f = varargin{1};
+                    end
+                otherwise
+                    if isa(varargin{1}, 'double')
+                        f = get(varargin{1});
+                    end
+                    if isa(varargin{1}, 'struct')
+                        f = varargin{1};
+                    end
+                    if mod(numel(varargin),2)~=1
+                        error('Invalid number of arguments')
+                    else
+                        for i=2:2:numel(varargin)
+                            if strcmp('strip', varargin{i})
+                                strip_style = varargin{i+1};
+                            end
+                            if strcmp('name', varargin{i})
+                                plot_name = varargin{i+1};
+                            end
+                            if strcmp('open', varargin{i})
+                                open_browser = varargin{i+1};
+                            end
+                        end
+                    end
+             end
+            
+             %convert figure into data and layout data structures
+            [data, layout, title] = convertFigure(f, strip_style);
+            
+            if numel(title)>0 && strcmp('untitled', plot_name)
+                plot_name = title;
+            end
+            
+            % send graph request
+            origin       = 'plot';
+            structargs   = struct('layout', layout, 'filename',plot_name, 'fileopt', 'overwrite');
+            obj.Response = makecall(data, obj.User, obj.Key, origin, structargs);
+            
+            if open_browser
+                status = dos(['open ' obj.Response.url ' > nul 2> nul']);
+                if status==1
+                    status = dos(['start ' obj.Response.url ' > nul 2> nul']);
+                end
+            end
+            
         end
         
         function obj = savecredentials(obj)
@@ -42,10 +122,13 @@ classdef plotly
             savecredentials(obj.User,obj.Key)
         end
     end
+    
     methods(Access = private)
         
-        % SIGNUP
         function obj = signup(obj)
+            % SIGNUP
+            
+            % Call external
             response = signup;
             if isempty(response), return, end
             obj.User = response.un;
@@ -54,6 +137,7 @@ classdef plotly
         
         function obj = loadcredentials(obj)
             % LOADCREDENTIALS
+            
             % Credentials file name
             userhome = getuserdir();
             if ispc
@@ -91,7 +175,5 @@ classdef plotly
             
         end
     end
-    
-    
-end
 
+end
