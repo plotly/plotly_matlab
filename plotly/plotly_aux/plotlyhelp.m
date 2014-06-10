405d477444f286fe99892ec4230ed8e517f6d399
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-06-10 12:39:52 -0400

    add return struct

--- plotly/plotly_aux/plotlyhelp.m
+++ plotly/plotly_aux/plotlyhelp.m
@@ -1,10 +1,11 @@
-function plotlyhelp(varargin)
+function plotlyref = plotlyhelp(varargin)
 % [EX]: plotlyhelp('scatter','fill'); 
 % [TODO]: make graph objects separate classes (a la Python API) 
 % [TODO]: better graph object descriptions 
  
 %converts graph_obj_meta.json to struct/cell array and outputs key
-plotlyref = loadjson(fileread('graph_objs_meta.json'));
+pr = loadjson(fileread('graph_objs_meta.json'));
+pr.online = 'Access the online docs!'; 
 %only display relevant fields. 
 %plotlyref = struct('scatter',plotlyref.scatter,'bar',plotlyref.bar,...
                 %'box',plotlyref.box, 'histogram',plotlyref.histogram, 'heatmap',plotlyref.heatmap, 'contour',plotlyref.contour,... 
@@ -12,22 +13,22 @@ plotlyref = loadjson(fileread('graph_objs_meta.json'));
 try
     switch length(varargin)
         case 0
-            plotlyref
+            plotlyref = pr; 
         case 1
             if(strcmpi('online',varargin{1}));
                 web('http://plot.ly/matlab/','-browser')
             else
-                plotlyref.(lower(varargin{1}))
+                plotlyref = pr.(lower(varargin{1})); 
             end 
         case 2
-               plotlyref.(lower(varargin{1})).(lower(varargin{2}))
+               plotlyref = pr.(lower(varargin{1})).(lower(varargin{2})); 
         case 3
-                plotlyref.(lower(varargin{1})).(lower(varargin{2})).(lower(varargin{3}))
+                plotlyref = pr.(lower(varargin{1})).(lower(varargin{2})).(lower(varargin{3})); 
         case 4
-                plotlyref.(lower(varargin{1})).(lower(varargin{2})).(lower(varargin{3})).(lower(varargin{4}))
+                plotlyre = pr.(lower(varargin{1})).(lower(varargin{2})).(lower(varargin{3})).(lower(varargin{4})); 
         %does the struct nesting ever go beyond 5 ?
         case 5
-                plotlyref.(lower(varargin{1})).(lower(varargin{2})).(lower(varargin{3})).(lower(varargin{4})).(lower(varargin{5})) 
+                plotlyref = pr.(lower(varargin{1})).(lower(varargin{2})).(lower(varargin{3})).(lower(varargin{4})).(lower(varargin{5})); 
     end
     
 catch exception
