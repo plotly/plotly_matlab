32204956c81daa1d185893e576c0dad10c3f72d9
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-06-10 12:41:15 -0400

    fix file path, add stream vars

--- plotly/saveplotlycredentials.m
+++ plotly/saveplotlycredentials.m
@@ -1,59 +1,58 @@
-function saveplotlycredentials(username, api_key, extra_struct)
-    % Save plotly authentication credentials.
-    % Plotly credentials are saved as JSON strings
-    % in ~/.plotly/.credentials
-    % extra is a struct
+function saveplotlycredentials(username, api_key, stream_key)
+% Save plotly authentication credentials.
+% Plotly credentials are saved as JSON strings
+% in ~/.plotly/.credentials
 
-    % if the credentials file exists, then load it up
-    try
-      creds = loadplotlycredentials();
-    catch
-      creds = struct();
-    end
+% if the credentials file exists, then load it up
+try
+    creds = loadplotlycredentials();
+catch
+    creds = struct();
+end
 
-    % Create the .plotly folder
-    userhome = getuserdir();
-    plotly_credentials_folder = [userhome '/.plotly'];
-    [status, mess, messid] = mkdir(plotly_credentials_folder);
-    if (status == 0)
-        if(~strcmp(messid, 'MATLAB:MKDIR:DirectoryExists'))
-            error('plotly:savecredentials',...
-                 ['Error saving credentials folder at ' ...
-                  plotly_credentials_folder ': '...
-                  mess ', ' messid '. Get in touch at ' ...
-                  'chris@plot.ly for support.']);
-        end
-    end
-    plotly_credentials_file = [plotly_credentials_folder '/.credentials'];
+% Create the .plotly folder
+userhome = getuserdir();
+plotly_credentials_folder = fullfile(userhome,'.plotly');
+[status, mess, messid] = mkdir(plotly_credentials_folder);
 
-    fileID = fopen(plotly_credentials_file, 'w');
-    if(fileID == -1)
+if (status == 0)
+    if(~strcmp(messid, 'MATLAB:MKDIR:DirectoryExists'))
         error('plotly:savecredentials',...
-              ['Error opening credentials file at '...
-               plotly_credentials_file '. Get in touch at '...
-               'chris@plot.ly for support.']);
+            ['Error saving credentials folder at ' ...
+            plotly_credentials_folder ': '...
+            mess ', ' messid '. Get in touch at ' ...
+            'chris@plot.ly for support.']);
     end
+end
 
-    if nargin < 3
-      creds.username = username;
-      creds.api_key  = api_key;
-    else
-      creds.username = username; 
-      creds.api_key = api_key; 
-      % merge extra into creds
-      % remove overlapping fields from first struct
-      updated_creds = rmfield(creds, intersect(fieldnames(creds), fieldnames(extra_struct)));
-      % obtain all unique names of remaining fields
-      names = [fieldnames(updated_creds); fieldnames(extra_struct)];
-      % merge both structs
-      updated_creds = cell2struct([struct2cell(updated_creds); struct2cell(extra_struct)], names, 1);
-      creds = updated_creds;
-    end
+plotly_credentials_file = fullfile(plotly_credentials_folder,'.credentials');
 
-    creds_string = m2json(creds);
+fileIDCred = fopen(plotly_credentials_file, 'w');
 
-    fprintf(fileID,'%s',creds_string);
+if(fileIDCred == -1)
+    error('plotly:savecredentials',...
+        ['Error opening credentials file at '...
+        plotly_credentials_file '. Get in touch at '...
+        'chris@plot.ly for support.']);
+end
 
-    fclose(fileID);
+switch nargin
+    case 2
+        creds.username = username;
+        creds.api_key  = api_key;
+    case 3
+        creds.username = username;
+        creds.api_key = api_key;
+        creds.stream_key = stream_key;
+    otherwise %need to specify both the username and api_key
+        error('plotly:savecredentials',...
+        'Please specify your username and api_key');   
+end
+
+creds_string = m2json(creds);
+
+%write the json strings to the cred file
+fprintf(fileIDCred,'%s',creds_string);
+fclose(fileIDCred);
 
 end
