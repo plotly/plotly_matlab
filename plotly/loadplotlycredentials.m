37ef523ee3d7ed566b6040479e861d998a1ba1ce
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-06-10 12:39:08 -0400

    fix filepath

--- plotly/loadplotlycredentials.m
+++ plotly/loadplotlycredentials.m
@@ -1,7 +1,7 @@
 function creds = loadplotlycredentials()
 
     userhome = getuserdir();
-    plotly_credentials_file = [userhome '/.plotly/.credentials'];
+    plotly_credentials_file = fullfile(userhome,'.plotly','.credentials');
 
     % check if credentials exist
     if ~exist(plotly_credentials_file, 'file')
@@ -13,18 +13,17 @@ function creds = loadplotlycredentials()
               'For more help, see https://plot.ly/MATLAB or contact '...
               'chris@plot.ly.']);
     end
-
-    fileID = fopen(plotly_credentials_file, 'r');
-    if(fileID == -1)
+    
+    fileIDCred = fopen(plotly_credentials_file, 'r');
+    
+    if(fileIDCred == -1)
         error('plotly:loadcredentials', ...
               ['There was an error reading your credentials file at '...
                plotly_credentials_file '. Contact chris@plot.ly for support.']);
     end
 
-    creds_string_array = fread(fileID, '*char');
-
-    creds_string = sprintf('%s',creds_string_array);
-
+    creds_string_array = fread(fileIDCred, '*char');
+    creds_string = sprintf('%s',creds_string_array); 
     creds = loadjson(creds_string);
-
+    
 end
