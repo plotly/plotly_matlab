a0710f1041c0c1c12de8e8d23ff202ab09421fc0
Chris P <christopher.parmer@gmail.com>
2014-06-30 18:57:35 -0400

    update tests with new filetypes

--- plotly/tests/test_saveplotlyfig.m
+++ plotly/tests/test_saveplotlyfig.m
@@ -6,13 +6,12 @@ images = [0, 1, 2, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,...
           33, 40, 42, 43, 44, 45, 50, 53, 56, 61, 62];
 test_failures_json = [26, 36, 49];
 test_failures_toolarge = [39];
-signin(test_account, test_account_key);
 
 formats = {'png', 'pdf', 'jpeg', 'svg'};
 for i=1:length(images)
     disp(['testing https://plot.ly/~PlotlyImageTest/' num2str(images(i)) '.' formats{j}])
     figure = getplotlyfig(test_account, images(i));
     for j=1:length(formats)
-        saveplotlyfig(figure, ['test_saveplotlyfig_images/' 'PlotlyImageTest_' num2str(images(i))], formats{j});
+        saveplotlyfig(figure, ['test_saveplotlyfig_images/' 'PlotlyImageTest_' num2str(images(i)) '.' formats{j}], formats{j});
     end
 end
