77d8a45f1ee7dcd9ba1aa2a2bea7be85cb854453
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-07-07 00:09:25 -0400

    fixed \text removal

--- plotly/fig2plotly_aux/stripTitle.m
+++ plotly/fig2plotly_aux/stripTitle.m
@@ -1,9 +1,14 @@
 function stripped = stripTitle(title)
-stripped = title; 
+stripped = title;
 stripped(regexp(stripped,'\$')) = '';
-stripped(regexp(stripped,'\\text'):regexp(stripped,'\\text')+length('\text')) = ''; 
-stripped(regexp(stripped,'{')) =''; 
+tloc = regexp(stripped,'\\text');
+try
+    for i = 1:length(tloc)
+        stripped(tloc(i) - (i-1)*length('\text'):tloc(i)+length('\text')-1-(i-1)*length('\text') ) = '';
+    end
+end
+stripped(regexp(stripped,'{')) ='';
 stripped(regexp(stripped,'}')) = '';
-stripped(regexp(stripped,'\\')) = ''; 
-stripped(regexp(stripped,' ')) = ''; 
+stripped(regexp(stripped,'\\')) = '';
+stripped(regexp(stripped,' ')) = '';
 end
