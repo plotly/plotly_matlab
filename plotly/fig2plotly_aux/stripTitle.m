153058eec20e6196f7ba93c2f6bf86b61d5fec2d
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-07-07 17:22:04 -0400

    added stripping of ^ and _ chars.

--- plotly/fig2plotly_aux/stripTitle.m
+++ plotly/fig2plotly_aux/stripTitle.m
@@ -1,14 +1,16 @@
 function stripped = stripTitle(title)
 stripped = title;
-stripped(regexp(stripped,'\$')) = '';
 tloc = regexp(stripped,'\\text');
 try
     for i = 1:length(tloc)
         stripped(tloc(i) - (i-1)*length('\text'):tloc(i)+length('\text')-1-(i-1)*length('\text') ) = '';
     end
 end
+stripped(regexp(stripped,'\$')) = '';
 stripped(regexp(stripped,'{')) ='';
 stripped(regexp(stripped,'}')) = '';
 stripped(regexp(stripped,'\\')) = '';
+stripped(regexp(stripped,'\^')) = '';
+stripped(regexp(stripped,'_')) = '';
 stripped(regexp(stripped,' ')) = '';
 end
