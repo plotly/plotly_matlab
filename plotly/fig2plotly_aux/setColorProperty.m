967ce7683926a87a15c30d66f4ff58e44eae1f81
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-07-11 12:10:11 -0400

    [FIX] setColorProperty prop = 'auto' handling

--- plotly/fig2plotly_aux/setColorProperty.m
+++ plotly/fig2plotly_aux/setColorProperty.m
@@ -1,6 +1,7 @@
-function color = setColorProperty(prop, color_ref, limits, colormap)
+function color = setColorProperty(prop, color_ref, limits, colormap,mh)
 
 color = {};
+
 %if one direct color
 if isa(prop, 'double')
     if numel(prop)==3
@@ -14,7 +15,7 @@ if strcmp(prop, 'none')
 end
 
 %if color defined by map
-if strcmp(prop, 'flat') || strcmp(prop, 'auto')
+if strcmp(prop, 'flat')
     %if direct color mapping
     if size(color_ref,2)==3
         color = cell(1, size(color_ref,1));
@@ -23,9 +24,27 @@ if strcmp(prop, 'flat') || strcmp(prop, 'auto')
         end
     else %if indirect color mapping
         color = mapColors(color_ref, limits, colormap);
-        
     end
 end
 
+%if color defined by axes (or figure)
+try
+    if strcmp(prop, 'auto')
+        ah = ancestor(mh.Parent,'axes');
+        if isfield(get(ah),'Color')
+            axCol = get(ah,'Color'); 
+            switch axCol 
+                case 'none'
+                    fh = ancestor(mh.Parent,'figure'); 
+                    figCol = get(fh,'Color'); 
+                    color{1}  = parseColor(figCol); 
+                otherwise
+                    color{1} = parseColor(axCol); 
+            end
+        end
+    end
+catch
+    color{1} = [0 0 1];
+end
 
 end
