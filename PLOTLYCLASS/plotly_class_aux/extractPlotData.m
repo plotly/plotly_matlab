59f5184a6ccc41593e8e111369f5b3452603b29b
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-07-15 14:34:44 -0400

    [ADD] XData YData extraction

--- PLOTLYCLASS/plotly_class_aux/extractPlotData.m
+++ PLOTLYCLASS/plotly_class_aux/extractPlotData.m
@@ -1,7 +1,27 @@
 function data = extractPlotData(obj)
-%check class of object
-
-
-
+plot_data = get(obj.State.DataHandle(obj.State.CurrentDataHandleIndex)); 
+data{obj.State.CurrentDataHandleIndex}.x = plot_data.XData;
+data{obj.State.CurrentDataHandleIndex}.y = plot_data.XData; 
+% data{obj.CurrentDataHandleIndex}.r; 
+% data{obj.CurrentDataHandleIndex}.t;
+% data{obj.CurrentDataHandleIndex}.mode;
+% data{obj.CurrentDataHandleIndex}.name;
+% data{obj.CurrentDataHandleIndex}.text;
+% data{obj.CurrentDataHandleIndex}.error_y;
+% data{obj.CurrentDataHandleIndex}.error_x;
+% data{obj.CurrentDataHandleIndex}.marker;
+% data{obj.CurrentDataHandleIndex}.line;
+% data{obj.CurrentDataHandleIndex}.connectgaps;
+% data{obj.CurrentDataHandleIndex}.fill;
+% data{obj.CurrentDataHandleIndex}.fillcolor;
+% data{obj.CurrentDataHandleIndex}.opacity;
+% data{obj.CurrentDataHandleIndex}.textfont;
+% data{obj.CurrentDataHandleIndex}.textposition;
+% data{obj.CurrentDataHandleIndex}.xaxis;
+% data{obj.CurrentDataHandleIndex}.yaxis;
+% data{obj.CurrentDataHandleIndex}.showlegend;
+% data{obj.CurrentDataHandleIndex}.stream;
+% data{obj.CurrentDataHandleIndex}.visible;
+% data{obj.CurrentDataHandleIndex}.type;
 end
 
