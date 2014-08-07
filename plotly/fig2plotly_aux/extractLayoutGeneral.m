54eb0383b9e0ea7a059a096bd58035a20668d5f3
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-08-07 17:53:06 -0400

    [FIX] layout margin calculation. Takes into account the units of the plot. [ADD] 50% increase in size of plot for nicer looking conversions

--- plotly/fig2plotly_aux/extractLayoutGeneral.m
+++ plotly/fig2plotly_aux/extractLayoutGeneral.m
@@ -1,43 +1,67 @@
 function layout = extractLayoutGeneral(f, layout, strip_style)
 
 %General attributes of the layout.
-%TODO - incorporate JSON GRAPH REF
+
+
 if strip_style
     layout.autosize = true;
     layout.margin.l=40;
     layout.margin.r=40;
     layout.margin.t=50;
     layout.margin.b=40;
-    layout.margin.pad=2;
+    layout.margin.pad=0;
     
 else
-    layout.margin.l=0;
-    layout.margin.r=0;
-    layout.margin.t=5;
-    layout.margin.b=0;
-    layout.margin.pad=0;
-    layout.autosize = false;
+    
+    try
+        fhan = get(f.Children(1),'Parent');
+        units = get(fhan,'Units');
+        set(fhan,'Units','pixels');
+       
+        %update f to reflect units
+        f = get(fhan);
+        
+        %extract the margins (extreme plot margins if subplots)
+        [l, r, b, t] = extractMargins(fhan);
+        
+        %title margin adjust
+        titleMinMar = 80; 
+        
+        layout.margin.l=l;
+        layout.margin.r=l; %add symmetry to avoid title misalignment
+        layout.margin.t=max(t,titleMinMar);
+        layout.margin.b=b;
+        layout.margin.pad=0;
+        layout.autosize = false;
+        
+        %reset the units 
+        set(fhan,'Units',units);
+        
+    catch 
+        layout.margin.l=40;
+        layout.margin.r=40;
+        layout.margin.t=80;
+        layout.margin.b=40;
+        layout.margin.pad=0;
+        layout.autosize = false;   
+    end
 end
 
-layout.width = f.Position(3);
-layout.height = f.Position(4)+layout.margin.t;
+%size increase (percentage); 
+perSizeInc = 0.50; 
 
-%ADD PAGE/PLOT COLOR - NOT USED RIGHT NOW 
-% if(~strip_style)
-%     %page color
-%     try
-%         layout.paper_bgcolor = parseColor(f.Color);
-%     end
-%     
-%     %plot colour
-%     for c = 1:length(f.Children)
-%         g = get(f.Children(c));
-%         try
-%             if(~strcmp(parseColor(g.Color),'rgb(255,255,255)'))
-%                 layout.plot_bgcolor = parseColor(g.Color);
-%             end
-%         end
-%     end
-% end
+try
+    fhan = get(f.Children(1),'Parent');
+    units = get(fhan,'Units');
+    set(fhan,'Units','pixels');
+    %update f to reflect units
+    f = get(fhan);
+    layout.width = f.Position(3)*(1 + perSizeInc); 
+    layout.height = f.Position(4)*(1 + perSizeInc);
+    set(fhan,'Units',units);
+catch
+    layout.width = f.Position(3)*(1 + perSizeInc);
+    layout.height = f.Position(4)*(1 + perSizeInc);
+end
 
 end
