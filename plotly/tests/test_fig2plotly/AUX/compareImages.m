7de9cbdcd25548b18df278323c3d4aa2ce11339f
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-08-07 17:56:11 -0400

    [ADD] markers to indicate location of differences in the compared images for fig2plotly testing

--- plotly/tests/test_fig2plotly/AUX/compareImages.m
+++ plotly/tests/test_fig2plotly/AUX/compareImages.m
@@ -6,7 +6,7 @@ imagesBase = dir([baseDir '/*.' ext]);
 imagesTest = dir([testDir '/*.' ext]);
 
 if(length(imagesBase)~=length(imagesTest))
-    fprintf(['\n\nWARNING: please make suer the same number ', ...
+    fprintf(['\n\nWARNING: please make sure the same number ', ...
         'of images are in both directories and try again!\n\n']);
     return
 end
@@ -27,18 +27,25 @@ for n = 1:length(imagesBase)
             % take the difference of the two images
             diffMat = abs(A-B);
             % plot the second image compared 
-            diffIm = imagesc(diffMat); 
+            hold on
+            imagesc(B); 
+            %add color marker to indicate difference
+            spy(mean(diffMat,3),'ro',10)
+            hold off 
             % save the diff image 
-            fn = [imagesTest(n).name '_diff']; 
+            imname = imagesTest(n).name; 
+            fn = [imname(1:end-length(ext)) '_diff']; 
             % use plotly to create a plot of the image
-            diffImFig = ancestor(diffIm,'figure'); 
-            resp = fig2plotly(diffImFig,'name',fn); 
+            %diffImFig = ancestor(diffIm,'figure'); 
+            resp = fig2plotly(gcf,'name',fn,'strip',0); 
             plotly_url_diff = resp.url; 
             fsIndDiff= strfind(plotly_url_diff,'/');
             %get the newly created plotly fig 
             plotlyfigDiff = getplotlyfig(un, plotly_url_diff(fsIndDiff(end)+1:end));
             %save plotly image
-            saveplotlyfig(plotlyfigDiff,[testDir '/' imagesTest(n).name '_DIFF'],'png');   
+            saveplotlyfig(plotlyfigDiff,[testDir '/' imagesTest(n).name '_DIFF'],'png');
+            %close image 
+            close; 
         else
             log{n} = ['TESTED: ' imagesBase(n).name ' RESULT: PASSED \n'];
         end
