1f0e1dcbce55e58b57f072c3dd93013a507c6a3c
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-08-14 20:29:27 -0400

    [ADD] layout graphref skeleton

--- plotlyfigure_dev/plotly_class_aux/extractPlotLayout.m
+++ plotlyfigure_dev/plotly_class_aux/extractPlotLayout.m
@@ -1,4 +1,32 @@
-function obj = extractPlotLayout( obj )
+function obj = extractPlotLayout(obj)
+
+% title: 
+% titlefont: 
+% font: 
+% showlegend: 
+% autosize: 
+% width: 
+% height: 
+% xaxis: 
+% yaxis: 
+% legend: 
+% annotations: 
+% margin: 
+% paper_bgcolor: 
+% plot_bgcolor: 
+% hovermode: 
+% dragmode: 
+% separators: 
+% barmode: 
+% bargap: 
+% bargroupgap: 
+% boxmode: 
+% radialaxis: 
+% angularaxis: 
+% direction: 
+% orientation: 
+% hidesources: 
+
 
 end
 
