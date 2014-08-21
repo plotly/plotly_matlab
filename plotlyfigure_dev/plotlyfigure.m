97c6aea1f508447e1675a7d78a77538651e9f5ce
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-08-21 10:37:10 -0400

    [FIX] notify figure/axis/plot [ADD] fig2plotly method

--- plotlyfigure_dev/plotlyfigure.m
+++ plotlyfigure_dev/plotlyfigure.m
@@ -12,9 +12,10 @@ classdef plotlyfigure < handle
     
     %----CLASS PROPERTIES----%
     properties
-        Data; % data of the plot
-        Layout; % layout of the plot
+        data; % data of the plot
+        layout; % layout of the plot
         PlotOptions; % filename,fileopt,world_readable
+        PlotlyDefaults;
         UserData; % credentials/configuration
         Response; % response of making post request
         State; % state of plot (FIGURE/AXIS/PLOTS)
@@ -27,15 +28,19 @@ classdef plotlyfigure < handle
         
         %----CONSTRUCTOR---%
         function obj = plotlyfigure(varargin)
-            if mod(nargin,2) ~= 0
-                error(['Oops! It appears that you did not initialize the Plotly figure object using the required ',...
-                    '(,..''key'',''value'',...) input structure. Please try again or contact chuck@plot.ly',...
-                    ' for any additional help!']);
+            
+            %check input structure
+            if nargin > 1
+                if mod(nargin,2) ~= 0 && ~ishandle(varargin{1})
+                    error(['Oops! It appears that you did not initialize the Plotly figure object using the required ',...
+                        '(,..''key'',''value'',...) input structure. Please try again or contact chuck@plot.ly',...
+                        ' for any additional help!']);
+                end
             end
             
             % core Plotly elements
-            obj.Data = cell(1,1);
-            obj.Layout = {};
+            obj.data = {};
+            obj.layout = struct();
             
             % user experience
             obj.Verbose = false;
@@ -45,13 +50,27 @@ classdef plotlyfigure < handle
             obj.PlotOptions.Fileopt = 'new';
             obj.PlotOptions.World_readable = true;
             obj.PlotOptions.Open_URL = false;
-            obj.PlotOptions.Strip = true;
+            obj.PlotOptions.Strip = false;
             obj.PlotOptions.Visible = 'on';
+            
+            % plot option defaults (edit these for custom conversions)
+            obj.PlotlyDefaults.MinTitleMargin = 80;
+            obj.PlotlyDefaults.FigureIncreaseFactor = 1.5;
+            obj.PlotlyDefaults.MarginPad = 0;
+            
+            % intialize Plot HandleIndexMap
+            obj.State.Plot.HandleIndexMap = {};
+            
+            % intialize Axis HandleIndexMap
+            obj.State.Axis.HandleIndexMap = {};
             
             % figure handle generation properties
             obj.HandleGen.Offset = 30000;
             obj.HandleGen.MaxAttempts = 10000;
             obj.HandleGen.Size = 4;
+            
+            % initialize reference figure
+            obj.State.Figure.Reference = [];
             
             % check for some key/vals
             for a = 1:2:nargin
@@ -74,10 +93,10 @@ classdef plotlyfigure < handle
                     obj.PlotOptions.Visible = varargin{a+1};
                 end
                 if(strcmpi(varargin{a},'layout'))
-                    obj.Layout= varargin{a+1};
+                    obj.layout= varargin{a+1};
                 end
                 if(strcmpi(varargin{a},'data'))
-                    obj.Data = varargin{a+1};
+                    obj.data = varargin{a+1};
                 end
             end
             
@@ -92,31 +111,39 @@ classdef plotlyfigure < handle
             
             % generate figure and handle
             fig = figure(obj.generateUniqueHandle);
-            
             % default figure
             set(fig,'Name','PLOTLY FIGURE','color',[1 1 1],'ToolBar','none','NumberTitle','off','Visible',obj.PlotOptions.Visible);
-            
             % figure state
             obj.State.Figure.Handle = fig;
-            obj.State.Figure.NumAxis = 0;
+            obj.State.Figure.NumAxes = 0;
+            obj.State.Figure.NumPlots = 0;
             
             % add figure listeners
             obj.addFigureListeners;
             
-            % add default axis
-            axes;
+            % notify new figure upon creation
+            obj.notifyNewFigure(fieldnames(get(obj.State.Figure.Handle)));
             
-            %addlistener(obj.State.Figure.Handle,'Name','PreSet',@(src,event,test)updateTest(obj,src,event,'hello'));
-            
-            % destroy obj upon figure deletion
-            set(obj.State.Figure.Handle,'DeleteFcn',@obj.deletePlotlyFigure);
+            % check to see if the first argument is a figure
+            if nargin > 0
+                if ishandle(varargin{1})
+                    obj.State.Figure.Reference.Handle = varargin{1};
+                    obj.fig2plotly;
+                else
+                    % add default axis
+                    axes;
+                end
+            else
+                % add default axis
+                axes;
+            end
             
             % plot response
             obj.Response = {};
         end
     end
     
-    methods (Access = private)
+    methods
         
         %----GENERATE UNIQUE FIGURE HANDLE----%
         function figureHandle = generateUniqueHandle(obj)
@@ -134,11 +161,6 @@ classdef plotlyfigure < handle
                 end
             end
         end
-
-        %----GET CURRENT AXIS INDEX----%
-        function currentAxisIndex = getCurrentAxisIndex(obj)
-            currentAxisIndex = find([obj.State.Axis(:).Handle] == get(obj.State.Figure.Handle,'CurrentAxes'));
-        end
         
         %----GET CURRENT AXIS HANDLE----%
         function currentAxisHandle = getCurrentAxisHandle(obj)
@@ -150,23 +172,40 @@ classdef plotlyfigure < handle
             currentPlotHandle = obj.State.Plot.Handle;
         end
         
-        %----DELETE PLOTLY FIGURE OBJECT----%
-        function obj = deletePlotlyFigure(obj,~,~)
-            delete(obj);
+        %----GET CURRENT AXIS INDEX ----%
+        function currentAxisIndex = getCurrentAxisIndex(obj)
+            currentAxisIndex = find(cellfun(@(h)eq(h,obj.getCurrentAxisHandle),obj.State.Axis.HandleIndexMap));
+        end
+        
+        %----GET CURRENT DATA INDEX ----%
+        function currentDataIndex = getCurrentDataIndex(obj)
+            currentDataIndex = find(cellfun(@(h)eq(h,obj.getCurrentPlotHandle),obj.State.Plot.HandleIndexMap));
         end
         
         %----SEND PLOT REQUEST----%
         function obj = plotly(obj)
             %args
             args = obj.PlotOptions;
-            %data
-            data = obj.Data;
             %layout
-            args.layout = obj.Layout;
+            args.layout = obj.layout;
             %send to plotly
-            response = plotly(data,args);
+            response = plotly(obj.data,args);
             %update response
             obj.Response = response;
+            %ouput url as hyperlink in command window if possible
+            try
+                desktop = com.mathworks.mde.desk.MLDesktop.getInstance;
+                editor = desktop.getGroupContainer('Editor');
+                if(~strcmp(response.url,'') && ~isempty(editor));
+                    fprintf(['\nLet''s have a look: <a href="matlab:openurl(''%s'')">' response.url '</a>\n\n'],response.url)
+                end
+            end
+            
+        end
+        
+        %----CHECK FOR EMPTY TEXT HANDLE----%
+        function isem = emptyTextPlotHandle(obj)
+            isem = strcmp(get(obj.State.Plot.Handle,'type'),'text') && isempty(get(obj.State.Plot.Handle,'String'));
         end
         
         %----GET OBJ.STATE.FIGURE.HANDLE ----%
@@ -180,98 +219,145 @@ classdef plotlyfigure < handle
             %new child added (axes)
             addlistener(obj.State.Figure.Handle,'ObjectChildAdded',@obj.figureAddAxis);
             %figure field names
-            figfields = fieldnames(get(obj.State.Figure.Handle));
+            figfields = {'Position','Color'};
             %add listeners to the figure fields
             for n = 1:length(figfields)
-                addlistener(obj.State.Figure.Handle,figfields{n},'PreSet',@(src,event,prop)updateFigure(obj,src,event,figfields{n}));
+                addlistener(obj.State.Figure.Handle,figfields{n},'PostSet',@(src,event,prop)updateFigure(obj,src,event,figfields{n}));
             end
         end
         
         %----ADD AXES LISTENERS----%
         function obj = addAxisListeners(obj)
+            %axis field names
+            axisfields = fieldnames(get(obj.getCurrentAxisHandle));
             %new child added
             addlistener(obj.getCurrentAxisHandle,'ObjectChildAdded',@(src,event)axisAddPlot(obj,src,event));
-            %old child removed 
+            %old child removed
             addlistener(obj.getCurrentAxisHandle,'ObjectChildRemoved',@(src,event)axisRemovePlot(obj,src,event));
-            %figure field names
-            axisfields = fieldnames(get(obj.getCurrentAxisHandle));
-            %add listeners to the figure fields
-            for n = 1:length(axisfields)
-                addlistener(obj.getCurrentAxisHandle,axisfields{n},'PreSet',@(src,event,prop)updateAxis(obj,src,event,axisfields{n}));
-            end
+            addlistener(obj.getCurrentAxisHandle,'Position','PostSet',@(src,event,prop)updateAxis(obj,src,event,'Position'));
+            %             %add listeners to the figure fields
+            %             for n = 1:length(axisfields)
+            %                 addlistener(obj.getCurrentAxisHandle,axisfields{n},'PostSet',@(src,event,prop)updateAxis(obj,src,event,axisfields{n}));
+            %             end
         end
         
-         %----ADD PLOT LISTENERS----%
+        %----ADD PLOT LISTENERS----%
         function obj = addPlotListeners(obj)
-            %new child added
-            addlistener(obj.getCurrentPlotHandle,'ObjectChildAdded',@(src,event)axisAddPlot(obj,src,event));
-            %old child removed 
-            addlistener(obj.getCurrentAxisHandle,'ObjectChildRemoved',@(src,event)axisRemovePlot(obj,src,event));
-            %figure field names
+            %plot field names
             plotfields = fieldnames(get(obj.getCurrentPlotHandle));
             %add listeners to the figure fields
             for n = 1:length(plotfields)
-                addlistener(obj.getCurrentPlotHandle,plotfields{n},'PreSet',@(src,event,prop)updatePlot(obj,src,event,plotfields{n}));
+                addlistener(obj.getCurrentPlotHandle,plotfields{n},'PostSet',@(src,event,prop)updatePlot(obj,src,event,plotfields{n}));
             end
+        end
+        
+        %----NOTIFY THE FIGURE PROPERTIES----%
+        function obj = notifyNewFigure(obj,prop)
+            %axis field names
+            figfields = prop;
+            %add listeners to the figure fields
+            for n = 1:length(figfields)
+                fl = addlistener(obj.State.Figure.Handle,figfields{n},'PostGet',@(src,event,prop)updateFigure(obj,src,event,figfields{n}));
+                %notify the listener
+                get(obj.State.Figure.Handle,figfields{n});
+                %delete the listener
+                delete(fl);
+            end
+        end
+        
+        %----NOTIFY THE AXIS PROPERTIES----%
+        function obj = notifyNewAxis(obj,prop)
+            axisfields = prop;
+            %add listeners to the figure fields
+            for n = 1:length(axisfields)
+                al = addlistener(obj.getCurrentAxisHandle,axisfields{n},'PostGet',@(src,event,prop)updateAxis(obj,src,event,axisfields{n}));
+                %notify the listener
+                get(obj.getCurrentAxisHandle,axisfields{n});
+                %delete the listener
+                delete(al);
+            end
+        end
+        
+        %----NOTIFY THE PLOT PROPERTIES----%
+        function obj = notifyNewPlot(obj,prop)
+            % plot field names
+            plotfields = prop;
+            %add listeners to the figure fields
+            for n = 1:length(plotfields)
+                pl = addlistener(obj.getCurrentPlotHandle,plotfields{n},'PostGet',@(src,event,prop)updatePlot(obj,src,event,plotfields{n}));
+                %notify the listener
+                get(obj.getCurrentPlotHandle,plotfields{n});
+                %delete the listener
+                delete(pl);
+            end
+        end
+        
+        
+        %automatic figure conversion
+        function obj = fig2plotly(obj)
+            % create temp figure
+            tempfig = figure('Visible','off');
+            % find axes of reference figure
+            ax = findobj(obj.State.Figure.Reference.Handle,'Type','axes');
+            for a = 1:length(ax)
+                % copy them to tempfigure
+                axtemp = copyobj(ax(a),tempfig);
+                % clear the children
+                cla(axtemp,'reset');
+                % add axtemp to figure
+                axnew = copyobj(axtemp,obj.State.Figure.Handle);
+                % copy ax children to axtemp
+                copyobj(allchild(ax(a)),axnew);
+            end
+            delete(tempfig);
         end
         
         %----CALLBACK FUNCTIONS---%
         
         %----ADD AN AXIS TO THE FIGURE----%
         function obj = figureAddAxis(obj,~,~)
-            obj.State.Figure.NumAxis = obj.State.Figure.NumAxis + 1;
-            obj.State.Axis(obj.State.Figure.NumAxis).Handle = get(obj.State.Figure.Handle,'CurrentAxes');
-            % add listeners to the axis
-            obj.addAxisListeners;
+            % potential axis handle
+            obj.State.Axis.Handle = obj.getCurrentAxisHandle;
+            % check for typw axes
+            if strcmp(get(obj.State.Axis.Handle,'Type'),'axes')
+                % update the number of axes
+                obj.State.Figure.NumAxes = obj.State.Figure.NumAxes + 1;
+                % update the axis HandleIndexMap
+                obj.State.Axis.HandleIndexMap{obj.State.Figure.NumAxes} = obj.State.Axis.Handle;
+                % add listeners to the axis
+                obj.addAxisListeners;
+                % notify the creation of the axis
+                obj.notifyNewAxis(fieldnames(get(obj.getCurrentAxisHandle)));
+            end
         end
         
         %----REMOVE AN AXIS FROM THE FIGURE----%
         function obj = figureRemoveAxis(obj,~,~)
+            
         end
-
+        
         %----ADD A PLOT TO AN AXIS----%
         function obj = axisAddPlot(obj,~,event)
-            % update obj.Data
+            % plot handle
             obj.State.Plot.Handle = event.Child;
-            obj.State.Plot.Call = event.Child.classhandle.Name;
-            % add listeners to the plot 
-            obj.addPlotListeners; 
+            % catch to be hidden handle (why does this happen?)
+            if ~obj.emptyTextPlotHandle
+                % update the plot index
+                obj.State.Figure.NumPlots = obj.State.Figure.NumPlots + 1;
+                % update the HandleIndexMap
+                obj.State.Plot.HandleIndexMap{obj.State.Figure.NumPlots} = obj.State.Plot.Handle;
+                % plot call class
+                obj.State.Plot.Call = event.Child.classhandle.Name;
+                % add listeners to the plot
+                obj.addPlotListeners;
+                % notify the creation of the plot
+                obj.notifyNewPlot(fieldnames(get(obj.getCurrentPlotHandle)));
+            end
         end
-                
+        
         %----REMOVE A PLOT FROM AN AXIS----%
         function obj = axisRemovePlot(obj,src,event)
         end
-        
-        %----UPDATE FIGURE DATA/LAYOUT----%
-        function obj = updateFigure(obj,~,~,prop)
-            switch prop
-                case 'Name'
-                    disp('GOT NAME CHANGE');
-                otherwise
-                    %  disp(prop);
-            end
-        end
-        
-        %----UPDATE AXIS DATA/LAYOUT----%
-        function obj = updateAxis(obj,~,~,prop)
-            switch prop
-                case 'Color'
-                    disp('GOT COLOR CHANGE');
-                otherwise
-                    % disp(prop);
-            end
-        end
-        
-        %----UPDATE PLOT DATA/STYLE----%
-        function obj = updatePlot(obj,~,~,prop)
-            switch prop
-                case 'Color'
-                    disp('GOT COLOR CHANGE');
-                otherwise
-                    % disp(prop);
-            end
-        end
-        
     end
 end
 
