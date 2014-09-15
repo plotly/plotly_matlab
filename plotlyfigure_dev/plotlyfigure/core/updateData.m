05f277699762f9b62524e4093ee98a4bdb57eb48
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-09-15 17:45:12 -0400

    [ADD] date time axis adjustments

--- plotlyfigure_dev/plotlyfigure/core/updateData.m
+++ plotlyfigure_dev/plotlyfigure/core/updateData.m
@@ -1,26 +1,28 @@
 %----UPDATE PLOT DATA/STYLE----%
+
 function obj = updateData(obj, dataIndex)
 
 %-update plot based on plot call class-%
-
 switch obj.State.Plot(dataIndex).Class
     
-    %--Plotly supported MATLAB core objects--%
-    
+    %--CORE PLOT OBJECTS--%
     case 'image'
-        %updatePlotImage(obj);
+        updateImage(obj, dataIndex);
     case 'line'
         updateLineseries(obj, dataIndex);
     case 'patch'
-        %updatePatch(obj, dataIndex);
+        % check for histogram
+        if isHistogram(obj,dataIndex)
+            updateHistogram(obj,dataIndex);
+        else
+            updatePatch(obj, dataIndex);
+        end
     case 'rectangle'
-        %updateRectangle(obj,dataIndex);
+        updateRectangle(obj,dataIndex);
     case 'surface'
-        %updateSurface(obj,dataIndex);
-        % case 'text' ------------------------- > Handled by updateAnnotation
-        
-        %--Plotly supported MATLAB plot objects--%
-        
+        %updateSurface(obj,dataIndex); 
+
+        %-GROUP PLOT OBJECTS-%
     case 'areaseries';
         updateAreaseries(obj, dataIndex);
     case 'barseries'
@@ -45,10 +47,32 @@ switch obj.State.Plot(dataIndex).Class
         %updateSurfaceplot(obj,dataIndex);
         
         %--Plotly supported MATLAB group plot objects--%
-        
     case 'hggroup'
-        %updateHggroup(obj,dataIndex);
-    case 'hgtransorm'
-        %updatePlothgtransform(obj,dataIndex);
+        % check for boxplot
+        if isBoxplot(obj, dataIndex)
+            updateBoxplot(obj, dataIndex);
+        end
 end
+
+%----------------------------DATA CLEAN UP--------------------------------%
+
+%-AXIS INDEX-%
+axIndex = obj.getAxisIndex(obj.State.Plot(dataIndex).AssociatedAxis);
+
+%-AXIS DATA-%
+eval(['xaxis = obj.layout.xaxis' num2str(axIndex) ';']);
+eval(['yaxis = obj.layout.yaxis' num2str(axIndex) ';']);
+
+%-FIX X/Y DATA-%
+
+% check for xaxis dates
+if strcmpi(xaxis.type, 'date')
+    obj.data{dataIndex}.x =  convertDate(obj.data{dataIndex}.x);
+end
+
+% check for yaxis dates
+if strcmpi(yaxis.type, 'date')
+    obj.data{dataIndex}.y =  convertDate(obj.data{dataIndex}.y);
+end
+
 end
