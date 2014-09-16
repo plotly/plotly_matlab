0f51c3242517c63c147f11634b3154f4e666cea6
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-09-16 17:18:47 -0400

    [DELETE] unused helperfunctions

--- plotlyfigure_dev/plotlyfigure/aux/helpers/isValidDate.m
+++
@@ -1,8 +0,0 @@
-function valid = isValidDate(string_to_check)
-try
-    datevec(string_to_check);
-    valid = true;
-catch
-    valid = false;
-end
-end
--- plotlyfigure_dev/plotlyfigure/aux/helpers/isvalidText.m
+++
@@ -1,6 +0,0 @@
-function check = validText(obj)
-check = false;
-try
-    check = isstrprop(get(obj,'String'),'alpha'); 
-end
-end
