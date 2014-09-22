2ed4cc96f2d4d994b8f08ad8307104e2db8a405b
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-09-22 11:21:52 -0400

    [CLEAN] dataIndex to errorbarIndex

--- plotlyfigure_dev/plotlyfigure/aux/updateErrorbarseries.m
+++ plotlyfigure_dev/plotlyfigure/aux/updateErrorbarseries.m
@@ -1,4 +1,4 @@
-function obj = updateErrorbarseries(obj, dataIndex)
+function obj = updateErrorbarseries(obj, errorbarIndex)
 
 % type: ...[DONE]
 % symmetric: ...[DONE]
@@ -15,10 +15,10 @@ function obj = updateErrorbarseries(obj, dataIndex)
 %-------------------------------------------------------------------------%
 
 %-ERRORBAR STRUCTURE-%
-errorbar_data = get(obj.State.Plot.Handle);
+errorbar_data = get(obj.State.Plot(errorbarIndex).Handle);
 
 %-ERRORBAR CHILDREN-%
-errorbar_child = get(obj.State.Plot.Handle,'Children');
+errorbar_child = get(obj.State.Plot(errorbarIndex).Handle,'Children');
 
 %-ERROR BAR LINE CHILD-%
 errorbar_line_child_data = get(errorbar_child(2));
@@ -26,54 +26,49 @@ errorbar_line_child_data = get(errorbar_child(2));
 %-------------------------------------------------------------------------%
 
 %-UPDATE LINESERIES-%
-updateLineseries(obj, dataIndex);
-
-%-------------------------------------------------------------------------%
-
-%-errorbar mode-%
-obj.data{dataIndex}.mode = 'markers';
+updateLineseries(obj, errorbarIndex);
 
 %-------------------------------------------------------------------------%
 
 %-errorbar visible-%
-obj.data{dataIndex}.error_y.visible = true;
+obj.data{errorbarIndex}.error_y.visible = true;
 
 %-------------------------------------------------------------------------%
 
 %-errorbar type-%
-obj.data{dataIndex}.error_y.type = 'data';
+obj.data{errorbarIndex}.error_y.type = 'data';
 
 %-------------------------------------------------------------------------%
 
 %-errorbar symmetry-%
-obj.data{dataIndex}.error_y.symmetric = false;
+obj.data{errorbarIndex}.error_y.symmetric = false;
 
 %-------------------------------------------------------------------------%
 
 %-errorbar value-%
-obj.data{dataIndex}.error_y.array = errorbar_data.UData;
+obj.data{errorbarIndex}.error_y.array = errorbar_data.UData;
 
 %-------------------------------------------------------------------------%
 
 %-errorbar valueminus-%
-obj.data{dataIndex}.error_y.arrayminus = errorbar_data.LData;
+obj.data{errorbarIndex}.error_y.arrayminus = errorbar_data.LData;
 
 %-----------------------------!STYLE!-------------------------------------%
 
 if ~obj.PlotOptions.Strip
     %-errorbar thickness-%
-    obj.data{dataIndex}.error_y.thickness = errorbar_line_child_data.LineWidth;
+    obj.data{errorbarIndex}.error_y.thickness = errorbar_line_child_data.LineWidth;
     
     %-------------------------------------------------------------------------%
     
     %-errorbar width-%
-    obj.data{dataIndex}.error_y.width = obj.PlotlyDefaults.ErrorbarWidth;
+    obj.data{errorbarIndex}.error_y.width = obj.PlotlyDefaults.ErrorbarWidth;
     
     %-------------------------------------------------------------------------%
     
     %-errorbar color-%
     col = 255*errorbar_line_child_data.Color;
-    obj.data{dataIndex}.error_y.color = ['rgb(' num2str(col(1)) ',' num2str(col(2)) ',' num2str(col(3)) ')'];
+    obj.data{errorbarIndex}.error_y.color = ['rgb(' num2str(col(1)) ',' num2str(col(2)) ',' num2str(col(3)) ')'];
     
     %-------------------------------------------------------------------------%
 end
