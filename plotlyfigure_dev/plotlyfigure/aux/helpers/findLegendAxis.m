aa6e3c58d538c744c4906aeecc99be24f2311467
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-09-22 11:41:43 -0400

    [FIX] finding legend axis algorithm

--- plotlyfigure_dev/plotlyfigure/aux/helpers/findLegendAxis.m
+++ plotlyfigure_dev/plotlyfigure/aux/helpers/findLegendAxis.m
@@ -1,4 +1,4 @@
 function legendAxis = findLegendAxis(obj,legendHandle)
-legendAxisIndex = find(arrayfun(@(x)(eq(getappdata(x.Handle,'LegendPeerHandle'),legendHandle)),obj.State.Axis));
+legendAxisIndex = find(arrayfun(@(x)(isequal(handle(getappdata(x.Handle,'LegendPeerHandle')),legendHandle)),obj.State.Axis), 1);
 legendAxis = obj.State.Axis(legendAxisIndex).Handle; 
 end
