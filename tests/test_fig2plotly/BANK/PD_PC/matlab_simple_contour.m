3dc1eef4f7925584f0ab89f978ddfceb7c52d30c
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-09-22 11:53:05 -0400

    [DELETE] tests from development merge

--- tests/test_fig2plotly/BANK/PD_PC/matlab_simple_contour.m
+++
@@ -1,9 +0,0 @@
-fig = figure; 
-[X,Y,Z] = peaks;
-contour(X,Y,Z,20)
-
-% PLOTLY 
-p = plotlyfigure(gcf);
-plotly(p,false);
-plotly_url = p.Response.url; 
-
--- tests/test_fig2plotly/BANK/PD_PC/matlab_spy_chart.m
+++
@@ -1,9 +0,0 @@
-fig = figure; 
-B = bucky;
-spy(B)
-
-% PLOTLY 
-p = plotlyfigure(gcf);
-plotly(p,false);
-plotly_url = p.Response.url; 
-
