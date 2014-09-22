d3a3aa8359cceefee8462bc6439fe41643ddeb53
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-09-22 11:40:51 -0400

    [CLEAN] comments

--- plotlyfigure_dev/plotlyfigure/aux/updateImage.m
+++ plotlyfigure_dev/plotlyfigure/aux/updateImage.m
@@ -12,7 +12,7 @@ function obj = updateImage(obj, imageIndex)
 % reversescale: ...[DONE]
 % showscale: ...[DONE]
 % colorbar: ...[HANDLED BY COLORBAR]
-% zsmooth: ...[NOT HANDLED BY MATLAB]
+% zsmooth: ...[NOT SUPPORTED BY MATLAB]
 % opacity: -----------------------------------> [TODO]
 % xaxis: ...[DONE]
 % yaxis: ...[DONE]
@@ -83,8 +83,8 @@ end
 
 %-IMAGE Z-%
 if(size(image_data.CData,3) > 1)
-    % TODO: FIX THIS TO ALLOW FOR TRUE COLOUR SPECS. 
-    obj.data{imageIndex}.z = image_data.CData(:,:,1);   
+    % TODO: FIX THIS TO ALLOW FOR TRUE COLOUR SPECS.
+    obj.data{imageIndex}.z = image_data.CData(:,:,1);
 else
     obj.data{imageIndex}.z = image_data.CData;
 end
@@ -96,65 +96,69 @@ obj.data{imageIndex}.name = image_data.DisplayName;
 
 %-------------------------------------------------------------------------%
 
-%-IMAGE SHOWSCALE-%
-obj.data{imageIndex}.showscale = false;
-
-%-------------------------------------------------------------------------%
-
 %-IMAGE VISIBLE-%
 obj.data{imageIndex}.visible = strcmp(image_data.Visible,'on');
 
-%-------------------------------------------------------------------------%
+%------------------------------!STYLE!------------------------------------%
 
-%-IMAGE REVERSE SCALE-%
-
-obj.data{imageIndex}.reversecale = false;
-
-%-------------------------------------------------------------------------%
-
-%-ZAUTO-%
-
-obj.data{imageIndex}.zauto = false;
-
-%-------------------------------------------------------------------------%
-
-%-ZMIN-%
-
-obj.data{imageIndex}.zmin = axis_data.CLim(1);
-
-%-------------------------------------------------------------------------%
-
-%-ZMAX-%
-
-obj.data{imageIndex}.zmax = axis_data.CLim(2);
-
-%-------------------------------------------------------------------------%
-
-%-COLORSCALE (ASSUMES IMAGE CDATAMAP IS 'SCALED')-%
-
-colormap = figure_data.Colormap;
-
-for c = 1:length(colormap)
-    col =  255*(colormap(c,:));
-    obj.data{imageIndex}.colorscale{c} = {(c-1)/length(colormap), ['rgb(' num2str(col(1)) ',' num2str(col(2)) ',' num2str(col(3)) ')']};
+if ~obj.PlotOptions.Strip
+    
+    
+    %-IMAGE SHOWSCALE-%
+    obj.data{imageIndex}.showscale = false;
+    
+    %---------------------------------------------------------------------%
+    
+    %-IMAGE REVERSE SCALE-%
+    
+    obj.data{imageIndex}.reversecale = false;
+    
+    %---------------------------------------------------------------------%
+    
+    %-ZAUTO-%
+    
+    obj.data{imageIndex}.zauto = false;
+    
+    %---------------------------------------------------------------------%
+    
+    %-ZMIN-%
+    
+    obj.data{imageIndex}.zmin = axis_data.CLim(1);
+    
+    %---------------------------------------------------------------------%
+    
+    %-ZMAX-%
+    
+    obj.data{imageIndex}.zmax = axis_data.CLim(2);
+    
+    %---------------------------------------------------------------------%
+    
+    %-COLORSCALE (ASSUMES IMAGE CDATAMAP IS 'SCALED')-%
+    
+    colormap = figure_data.Colormap;
+    
+    for c = 1:length(colormap)
+        col =  255*(colormap(c,:));
+        obj.data{imageIndex}.colorscale{c} = {(c-1)/length(colormap), ['rgb(' num2str(col(1)) ',' num2str(col(2)) ',' num2str(col(3)) ')']};
+    end
+    
+    %---------------------------------------------------------------------%
+    
+    %-IMAGE SHOWLEGEND-%
+    leg = get(image_data.Annotation);
+    legInfo = get(leg.LegendInformation);
+    
+    switch legInfo.IconDisplayStyle
+        case 'on'
+            showleg = true;
+        case 'off'
+            showleg = false;
+    end
+    
+    obj.data{imageIndex}.showlegend = showleg;
+    
+    %---------------------------------------------------------------------%
+    
 end
-
-%-------------------------------------------------------------------------%
-
-%-IMAGE SHOWLEGEND-%
-leg = get(image_data.Annotation);
-legInfo = get(leg.LegendInformation);
-
-switch legInfo.IconDisplayStyle
-    case 'on'
-        showleg = true;
-    case 'off'
-        showleg = false;
-end
-
-obj.data{imageIndex}.showlegend = showleg;
-
-%-------------------------------------------------------------------------%
-
 end
 
