71098749674ab47b981ad4d24c3ce2cb54bde83d
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-09-22 18:10:09 -0400

    [FIX] plotlyhelp parse structure for new graph ref.

--- plotly/plotlyhelp.m
+++ plotly/plotlyhelp.m
@@ -1,24 +1,26 @@
 function plotlyref = plotlyhelp(varargin)
-% [EX]: plotlyhelp('scatter','fill'); 
+% [EX]: plotlyhelp('scatter','fill');
 
 %converts graph_obj_meta.json to struct/cell array and outputs key
 pr = loadjson(fileread('graph_objs_meta.json'));
-pr.online = 'Access the online docs!'; 
+pr.online = 'Access the online docs!';
 
 try
     switch length(varargin)
         case 0
-            plotlyref = pr; 
+            plotlyref = pr;
         case 1
             if(strcmpi('online',varargin{1}));
                 web('http://plot.ly/matlab/','-browser')
             else
-                plotlyref = pr.(lower(varargin{1})); 
-            end 
+                plotlyref = pr.(lower(varargin{1}));
+            end
         case 2
-                plotlyref = pr.(lower(varargin{1})).(lower(varargin{2})); 
-        case 3 %does the struct nesting ever go beyond 3 ?
-                plotlyref = pr.(lower(varargin{1})).(lower(varargin{2})).(lower(varargin{3})); 
+            plotlyref = pr.(lower(varargin{1})).(lower(varargin{2}));
+        case 3 
+            plotlyref = pr.(lower(varargin{1})).(lower(varargin{2})).(lower(varargin{3}));
+        case 4 %does the struct nesting ever go beyond 4?
+            plotlyref = pr.(lower(varargin{1})).(lower(varargin{2})).(lower(varargin{3})).(lower(varargin{4}));
     end
     
 catch exception
