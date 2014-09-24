20fcff4da878debd4878c564c186d6a22b99ee1d
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-09-24 09:41:04 -0400

    check for multiple baseline and hide extras if necessary

--- plotlyfigure_dev/plotlyfigure/aux/updateBaseline.m
+++ plotlyfigure_dev/plotlyfigure/aux/updateBaseline.m
@@ -3,9 +3,17 @@ function obj = updateBaseline(obj, baseIndex)
 %-------------------------------------------------------------------------%
 
 %-UPDATE LINESERIES-%
-updateLineseries(obj, baseIndex); 
+updateLineseries(obj, baseIndex);
+
+%-------------------------------------------------------------------------%
 
 %-SHOWLEGEND-%
-obj.data{baseIndex}.showlegend = obj.PlotlyDefaults.ShowBaselineLegend; 
+obj.data{baseIndex}.showlegend = obj.PlotlyDefaults.ShowBaselineLegend;
+
+%-CHECK FOR MULTIPLE BASELINES-%
+if ismultipleBaseline(obj,baseIndex)
+    %-hide baseline if mutliple-%
+    obj.data{baseIndex}.visible = false;
+end
 
 end
