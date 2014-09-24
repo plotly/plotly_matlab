cdb62d55cf4473b7231449605b7c0ea312832ca5
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-09-24 09:38:09 -0400

    add comment explaining overlap check logic

--- plotlyfigure_dev/plotlyfigure/aux/helpers/isOverlapping.m
+++ plotlyfigure_dev/plotlyfigure/aux/helpers/isOverlapping.m
@@ -1,5 +1,5 @@
 function [multipleaxis, overlap] = isOverlapping(obj, axIndex)
 % check axis overlap
 overlap = find(arrayfun(@(x)(isequal(get(x.Handle,'Position'),get(obj.State.Axis(axIndex).Handle,'Position'))),obj.State.Axis(1:axIndex)));
-multipleaxis = length(overlap) > 1;
+multipleaxis = length(overlap) > 1; %greater than 1 because obj.State.Axis(1:axIndex) will always match 
 end
