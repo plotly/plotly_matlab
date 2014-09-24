8626513c3dbebf353c9dc8032b4f3090dc673409
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-09-24 09:41:35 -0400

    pass object to stripkeys

--- plotlyfigure_dev/plotlyfigure/aux/helpers/stripkeys.m
+++ plotlyfigure_dev/plotlyfigure/aux/helpers/stripkeys.m
@@ -1,4 +1,7 @@
-function stripped = stripkeys(fields, fieldname, key, pr)
+function stripped = stripkeys(obj, fields, fieldname, key)
+
+%plorlt reference
+pr = obj.PlotlyReference; 
 
 % initialize output
 stripped = fields;
@@ -7,54 +10,64 @@ stripped = fields;
 fn = fieldnames(stripped);
 fnmod = fn;
 
-for d = 1:length(fn);
+try
     
-    % clean up axis keys
-    if any(strfind(fn{d},'xaxis')) || any(strfind(fn{d},'yaxis'))
-        fnmod{d} = fn{d}(1:length('_axis'));
-    end
-    
-    % keys:(object, style, plot_info, data)
-    keytype = getfield(pr,fieldname,'keymeta',fnmod{d},'key_type');
-    
-    % check for objects
-    if strcmp(keytype,'object')
+    for d = 1:length(fn);
         
-        % clean up font keys
-        if any(strfind(fn{d},'font'))
-            fnmod{d} = 'font';
+        % clean up axis keys
+        if any(strfind(fn{d},'xaxis')) || any(strfind(fn{d},'yaxis'))
+            fnmod{d} = fn{d}(1:length('_axis'));
         end
         
-        % handle annotations
-        if strcmp(fn{d},'annotations')
-            annot = getfield(stripped, fn{d});
-            fnmod{d} = 'annotation';
-            for a = 1:length(annot)
-                %recursive call to stripkeys
-                stripped.annotations{a} = stripkeys(annot{a},fnmod{d},key,pr);
+        % keys:(object, style, plot_info, data)
+        keytype = getfield(pr,fieldname,fnmod{d},'key_type');
+        
+        % check for objects
+        if strcmp(keytype,'object')
+            
+            % clean up font keys
+            if any(strfind(fn{d},'font'))
+                fnmod{d} = 'font';
             end
-        else
-            %recursive call to stripkeys
-            stripped = setfield(stripped, fn{d}, stripkeys(getfield(stripped, fn{d}),fnmod{d},key,pr));
+            
+            % handle annotations
+            if strcmp(fn{d},'annotations')
+                annot = getfield(stripped, fn{d});
+                fnmod{d} = 'annotation';
+                for a = 1:length(annot)
+                    %recursive call to stripkeys
+                    stripped.annotations{a} = stripkeys(obj, annot{a}, fnmod{d}, key);
+                end
+            else
+                %recursive call to stripkeys
+                stripped = setfield(stripped, fn{d}, stripkeys(obj, getfield(stripped, fn{d}), fnmod{d}, key));
+            end
+            
+            % look for desired key
+        elseif any(strcmp(keytype, key))
+            stripped = rmfield(stripped, fn{d});
         end
         
-    % look for desired key
-    elseif any(strcmp(keytype, key))
-        stripped = rmfield(stripped, fn{d});
     end
     
-end
-
-%----CLEAN UP----%
-remfn = fieldnames(stripped);
-
-for n = 1:length(remfn)
-    if isstruct(getfield(stripped,remfn{n}))
-        if isempty(fieldnames(getfield(stripped,remfn{n})))
+    %----CLEAN UP----%
+    remfn = fieldnames(stripped);
+    
+    for n = 1:length(remfn)
+        if isstruct(getfield(stripped,remfn{n}))
+            if isempty(fieldnames(getfield(stripped,remfn{n})))
+                stripped = rmfield(stripped,remfn{n});
+            end
+        elseif isempty(getfield(stripped,remfn{n}))
             stripped = rmfield(stripped,remfn{n});
         end
-    elseif isempty(getfield(stripped,remfn{n}))
-        stripped = rmfield(stripped,remfn{n});
     end
+    
+catch exception
+    
+    if obj.UserData.Verbose
+        fprintf(['\nWhoops! ' exception.message '\n\n']);
+    end
+    
 end
 
