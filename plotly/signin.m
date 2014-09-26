2c7d7d17b07fca0efd60e1474e7f738779dacd0b
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-09-26 09:22:07 -0400

    allow for plotly_domain to be specified using signup

--- plotly/signin.m
+++ plotly/signin.m
@@ -1,34 +1,39 @@
 function [un, key, domain] = signin(varargin)
-% SIGNIN(username, api_key)  Sign In to a plotly session
+% SIGNIN(username, api_key, plotly_domain)  Sign In to a plotly session
 %
-% See also plotly, plotlylayout, plotlystyle, signup
+% See also plotly, signup
 %
-% For full documentation and examples, see https://plot.ly/api
-%[TODO]: account for stream cred/config vars.
+% For full documentation and examples, see https://plot.ly/matlab/getting-started/
 
-    persistent USERNAME KEY PLOTLY_DOMAIN
-    if nargin==2 && ischar(varargin{1}) && ischar(varargin{2})
-        USERNAME = varargin{1};
-        KEY = varargin{2};
-        plotlysession('MATLAB API');
-        mlock;
-    elseif isempty(USERNAME) || isempty(KEY)
-        creds = loadplotlycredentials();
-        USERNAME = creds.username;
-        KEY = creds.api_key;
-    end
-    un = USERNAME;
-    key = KEY;
+persistent USERNAME KEY PLOTLY_DOMAIN
+if nargin > 1 && ischar(varargin{1}) && ischar(varargin{2})
+    USERNAME = varargin{1};
+    KEY = varargin{2};
+    plotlysession('MATLAB API');
+    mlock;
+elseif isempty(USERNAME) || isempty(KEY)
+    creds = loadplotlycredentials();
+    USERNAME = creds.username;
+    KEY = creds.api_key;
+end
+
+un = USERNAME;
+key = KEY;
+
+if nargin > 2 && ischar(varargin{3})
+    PLOTLY_DOMAIN = varargin{3};
+else
     if isempty(PLOTLY_DOMAIN)
         try
             config = loadplotlyconfig();
             PLOTLY_DOMAIN = config.plotly_domain;
-        catch
+        catch 
             % fails cuz either creds haven't been written yet
             % or because plotly_domain wasn't a key in the
             % creds file.
             PLOTLY_DOMAIN = 'https://plot.ly';
         end
     end
-    domain = PLOTLY_DOMAIN;
+end
+domain = PLOTLY_DOMAIN;
 end
