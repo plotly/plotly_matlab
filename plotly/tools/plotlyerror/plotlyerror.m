5136df1b4c802e0da436c3155153ec3a34203e46
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-09-30 14:48:29 -0400

    first plotlyerror

---
+++ plotly/tools/plotlyerror/plotlyerror.m
@@ -0,0 +1,16 @@
+function errormsg = plotlyerror(key)
+switch key
+    %--plotlyfigure constructor--%
+    case 'plotlyfigureConstructor:invalidInputs';
+        errormsg = ['\nOops! It appears that you did not initialize the Plotly figure object using the\n', ...
+            'required: >>  plotlyfigure(handle [optional],''property'',''value'',...) \n',...
+            'input structure. Please try again or contact chuck@plot.ly for any additional help!'];
+        %--saveplotlyfig invocation--%;
+    case 'plotlySaveImage:invalidInputs'
+        errormsg = ['\nOops! It appears that you did not invoke the saveplotlyfig function using the\n', ...
+            'required: >>  saveplotlyfig(plotly_figure, ...) input structure, where plotly_figure\n',...
+            'is of type cell (for data traces) or of type struct (with data and layout fields). \n',...
+            'Please try again or contact chuck@plot.ly for any additional help!'];
+        
+end
+end
