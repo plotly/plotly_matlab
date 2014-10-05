55a2c79b2a001ad4cbbe2f0036ac019324e3caac
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-10-05 00:30:20 -0400

    fix findplotlydirs plotly loc

--- plotly/tools/plotlyupdate/aux/helpers/findPlotlyDirs.m
+++ plotly/tools/plotlyupdate/aux/helpers/findPlotlyDirs.m
@@ -5,8 +5,8 @@ plotlyDirs = cell(1,length(plotlyScriptDirs));
 
 for d = 1:length(plotlyScriptDirs)
     %parse filepath string at the Plotly directory
-    plotlyLoc = strfind(fileparts(plotlyScriptDirs{d}),'/plotly');
-    plotlyDirs{d} = plotlyScriptDirs{d}(1:plotlyLoc);
+    plotlyLoc = strfind(fileparts(plotlyScriptDirs{d}),'plotly');
+    plotlyDirs{d} = fullfile(plotlyScriptDirs{d}(1:plotlyLoc-1),'plotly');
 end
 
 end
