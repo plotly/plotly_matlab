ef4a3bed84b1b01794a46ae7558e67a882600e89
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-10-06 12:14:14 -0400

    add is empty check for plotly dir location parse

--- plotly/plotly_setup_aux/findPlotlyDirs.m
+++ plotly/plotly_setup_aux/findPlotlyDirs.m
@@ -6,7 +6,9 @@ plotlyDirs = cell(1,length(plotlyScriptDirs));
 for d = 1:length(plotlyScriptDirs)
     %parse filepath string at the Plotly directory
     plotlyLoc = strfind(fileparts(plotlyScriptDirs{d}),'plotly');
-    plotlyDirs{d} = fullfile(plotlyScriptDirs{d}(1:plotlyLoc-1),'plotly');
+    if ~isempty(plotlyLoc)
+        plotlyDirs{d} = fullfile(plotlyScriptDirs{d}(1:plotlyLoc-1),'plotly');
+    end
 end
 
 end
