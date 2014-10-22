2a7f4699eff6a6bf2bf51e23b33ceca2cbf26d70
bronsolo <bronsolo@bronsolos-MacBook-Pro.local>
2014-10-22 15:49:55 -0400

    replace makeplotlygrid with plotlygrid add appendRows and appendCols methods

--- plotlygrid_dev/plotlygrid.m
+++ plotlygrid_dev/plotlygrid.m
@@ -6,10 +6,14 @@ classdef plotlygrid < dynamicprops & handle
     properties
         UserData;
         GridOptions;
-        GridData;
-        Response;
+        url;
+        warnings;
     end
     
+    properties (Hidden=true)
+        GridData;
+        File;
+    end
     %----CLASS METHODS----%
     methods
         
@@ -36,26 +40,27 @@ classdef plotlygrid < dynamicprops & handle
             %--parse data--%
             if isstruct(data)
                 if ~isempty(data)
-                    fields = fieldnames(data); 
+                    fields = fieldnames(data);
                     for f = 1:length(fields)
                         %format the GridData
-                        obj.GridData.cols.(fields{f}).data = getfield(data,fields{f}); 
+                        obj.GridData.cols.(fields{f}).data = data.(fields{f});
+                        obj.GridData.cols.(fields{f}).order = f;
                     end
                 end
             end
-           
+            
             %--check for Key/Value--%
             if mod(length(varargin),2)~=0
                 error('must be key value');
             end
             
             %--initialize GridData--%
-            obj.GridOptions.FileName = 'untitled'; 
-            obj.GridOptions.WorldReadable = true; 
-            obj.GridOptions.Parent = '0'; 
-            obj.GridOptions.ParentPath = ''; 
-            obj.GridOptions.OpenUrl = true; 
-            obj.GridOptions.ShowUrl = true; 
+            obj.GridOptions.FileName = 'untitled';
+            obj.GridOptions.WorldReadable = true;
+            obj.GridOptions.Parent = '0';
+            obj.GridOptions.ParentPath = '';
+            obj.GridOptions.OpenUrl = true;
+            obj.GridOptions.ShowUrl = true;
             
             %--parse variable arguments--%
             for n = 1:2:length(varargin)
@@ -71,67 +76,104 @@ classdef plotlygrid < dynamicprops & handle
                     case 'open'
                         obj.GridOptions.OpenUrl = varargin{n+1};
                     case 'show_url'
-                        obj.GridOptions.ShowUrl = varargin{n+1}; 
+                        obj.GridOptions.ShowUrl = varargin{n+1};
                 end
             end
             
             %--send grid to Plotly--%
-            obj.makePlotlyGrid; 
+            obj.upload;
             
-%             % post grid to plotly
-%             % resp = obj.makecall;
-%             resp.time = '123AB2'; 
-%             resp.power = 'J67003';
-%             
-%             % data fields
-%             datafields = fieldnames(data);
-%             
-%             
-%             % add dynamic properties
-%             for n = 1:length(datafields)
-%                 % add property field
-%                 addprop(obj,datafields{n});
-%                 % create column object
-%                 plotlycol = plotlycolumn(getfield(data,datafields{n}),'id', getfield(resp,datafields{n}));
-%                 % initialize property field
-%                 setfield(obj, datafields{n}, plotlycol);
-%             end
+            %--add dynamic properties--%
+            fields = fieldnames(data);
             
+            for d = 1:length(fields)
+                % add property field
+                addprop(obj,fields{d});
+                % create column object
+                plotlycol = plotlycolumn(data.(fields{d}), obj.File.cols{d}.name, obj.File.cols{d}.uid);
+                % initialize property field
+                obj.(fields{d}) = plotlycol;
+            end
         end
         
-        function obj = makePlotlyGrid(obj)
+        function obj = upload(obj)
             
-            platform = 'MATLAB'; %not supported yet
-            url = [obj.UserData.Domain '/v2/grids'];
-            
-            %--HEADERS--%
-            names = {'plotly-username','plotly-apikey','content-type','accept'};
-            values = {obj.UserData.UserName,obj.UserData.ApiKey,'application/json','*/*'};
-            [headers(1:length(names)).name] = names{1,:}; 
-            [headers(1:length(values)).value] = values{1,:}; 
+            platform = 'MATLAB';
+            endpoint = [obj.UserData.Domain '/v2/grids'];
             
             %-PAYLOAD-%
-            payload.data = m2json(obj.GridData); 
-            payload.filename = obj.GridOptions.FileName; 
-            payload.world_readable = obj.GridOptions.WorldReadable; 
-            payload.parent = obj.GridOptions.Parent; 
-            payload.parent_path = obj.GridOptions.ParentPath; 
+            payload.data = m2json(obj.GridData);
+            payload.filename = obj.GridOptions.FileName;
+            payload.world_readable = obj.GridOptions.WorldReadable;
             
-            if (is_octave)
-                % use octave super_powers
-                resp = urlread2(url, 'post', m2json(payload), headers);
-            else
-                % do it matlab way
-                resp = urlread2(url, 'Post', m2json(payload), headers);
-            end
+            encoder = sun.misc.BASE64Encoder();
+            encoded_un_key = char(encoder.encode(java.lang.String([obj.UserData.UserName, ':', obj.UserData.ApiKey]).getBytes()));
+            headers = struct('name', {'Authorization','plotly_client_platform','content-type','accept'}, 'value', {['Basic ' encoded_un_key], platform, 'application/json','*/*'});
+            
+            resp = urlread2(endpoint, 'Post', m2json(payload), headers);
             
             % check response
             response_handler(resp);
             
             % add to PlotOption
-            obj.Response = loadjson(resp);
+            response = loadjson(resp);
+            obj.File = response.file;
+            obj.url = response.file.web_url;
+            obj.warnings = response.warnings;
+            
+        end
+        
+        function obj = appendCols(obj, cols)
+            
+            %cellularize input 
+            if ~iscell(cols)
+                cols = {cols};
+            end
+           
+            platform = 'MATLAB';
+            endpoint = [obj.UserData.Domain '/v2/grids/' obj.File.fid '/col'];
+            
+            %-PAYLOAD-%
+            payload.cols = m2json(cols);
+            
+            encoder = sun.misc.BASE64Encoder();
+            encoded_un_key = char(encoder.encode(java.lang.String([obj.UserData.UserName, ':', obj.UserData.ApiKey]).getBytes()));
+            headers = struct('name', {'Authorization','plotly_client_platform','content-type','accept'}, 'value', {['Basic ' encoded_un_key], platform, 'application/json','*/*'});
+            
+            resp = urlread2(endpoint, 'Post', m2json(payload), headers);
+            
+            % check response
+            response_handler(resp);
+            
+            % structure resp
+            response = loadjson(resp);
+            
+%             for c = 1:length(name)
+%             % add property fields
+%             addprop(obj, name{c});
+%             
+%             % create column objects
+%             plotlycol = plotlycolumn(data, name, response.cols.uid);
+%             
+%             % initialize property fields
+%             obj.(fields{d}) = plotlycol;
+%             end
+        end
+        
+        function obj = appendRows(obj, data)
+            
+            platform = 'MATLAB'; %not supported yet
+            endpoint = [obj.UserData.Domain '/v2/grids/' obj.File.fid '/row'];
+            
+            %-PAYLOAD-%
+            payload.rows = m2json(data);
+            
+            encoder = sun.misc.BASE64Encoder();
+            encoded_un_key = char(encoder.encode(java.lang.String([obj.UserData.UserName, ':', obj.UserData.ApiKey]).getBytes()));
+            headers = struct('name', {'Authorization','plotly_client_platform','content-type','accept'}, 'value', {['Basic ' encoded_un_key], platform, 'application/json','*/*'});
+            
+            urlread2(endpoint, 'Post', m2json(payload), headers);
             
         end
     end
 end
-
