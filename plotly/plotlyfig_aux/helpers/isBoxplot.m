3cc4b6cc20ca86b993ab9e090ca07e9dc85b38b9
bronsolo <bronsolo@bronsolos-MacBook-Pro.local>
2014-10-28 09:02:22 -0400

    change any to is empty - to handle objects in new hG2

--- plotly/plotlyfig_aux/helpers/isBoxplot.m
+++ plotly/plotlyfig_aux/helpers/isBoxplot.m
@@ -1,3 +1,3 @@
 function check = isBoxplot(obj, boxIndex)
-   check = any([findobj(obj.State.Plot(boxIndex).Handle,'Tag', 'Box')' findobj(obj.State.Plot(boxIndex).Handle,'Tag', 'Outliers')']); 
+   check = ~isempty([findobj(obj.State.Plot(boxIndex).Handle,'Tag', 'Box')' findobj(obj.State.Plot(boxIndex).Handle,'Tag', 'Outliers')']); 
 end
