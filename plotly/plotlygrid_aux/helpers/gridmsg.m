3442a4aeb6a4a2b8d057528b8fd923e522448b1f
bronsolo <bronsolo@bronsolos-MacBook-Pro.local>
2014-11-02 11:25:24 -0500

    more gridmsgs

--- plotly/plotlygrid_aux/helpers/gridmsg.m
+++ plotly/plotlygrid_aux/helpers/gridmsg.m
@@ -8,16 +8,30 @@ switch key
             'For more help, see https://plot.ly/MATLAB or contact '...
             'chuck@plot.ly.\n\n'];
     case 'gridFilename:notValid'
-        errormsg = ['Oops! Please specify a valid filename'];
+        errormsg = ['\nOops! Please specify a valid filename.\n\n'];
+    case 'gridData:notFound'
+        errormsg = ['\nOops! No grid data was found to upload to Plotly.\n\n'];
+    case 'gridData:notValid'
+        errormsg = ['\nOops! The grid data must be contained within a struct.\n\n'];
+    case 'gridAppendRows:noGridId'
+        errormsg = ['\nOops! Please specify a gridId!\n\n'];
+    case 'gridAppendRows:invalidInput'
+        errormsg = ['\nOops! Please data input must be a matrix!\n\n'];
+    case 'gridAppendCols:noGridId'
+        errormsg = ['\nOops! Please specify a gridId!\n\n'];
+    case 'gridAppendCols:invalidInput'
+        errormsg = ['\nOops! Column input must be a struct!\n\n'];
     case 'gridFilename:alreadyExists'
-        errormsg = ['\nOops! A file already exists with this filename.\n'];
+        errormsg = ['\nOops! A file already exists with this filename.\n\n'];
     case 'gridInputs:notKeyValue'
         errormsg = ['\nOops! The variable argument inputs to plotlygrid.m ',...
-            'must be key value. \n'];
+            'must be key value. \n\n'];
     case 'gridCols:duplicateName'
-        errormsg = ['Oops! Please specify unique name for each column.'];
+        errormsg = ['\nOops! Please specify unique name for each column.\n\n'];
     case 'gridDelete:tooInvalidInputs'
-        errormsg = ['\nOops! you specified the wrong number of inputs!'];
+        errormsg = ['\nOops! you specified the wrong number of inputs!\n\n'];
+    case 'gridGet:invalidInputs'
+        errormsg = ['\nOops! you specified the wrong number of inputs!\n\n'];
     case 'gridGeneric:genericError'
-        errormsg = [''];
+        errormsg = ['\n'];
 end
