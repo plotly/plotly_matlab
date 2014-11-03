f5259c4dcaeda379f47f4f1ea217a7d441b3f95f
bronsolo <bronsolo@bronsolos-MacBook-Pro.local>
2014-11-03 10:51:04 -0500

    some plotlysearch testing

--- plotlysearch.m
+++ plotlysearch.m
@@ -1,3 +1,17 @@
 function resp = plotlysearch(query)
 
+%--user data--%
+[username, apikey] = signin;
+
+%--relative endpoint--%
+relative_endpoint = '/search';
+
+%-payload-%
+payload = query;
+
+%-caller-%
+caller = plotlyapiv2(username,apikey);
+caller.makecall('Get', relative_endpoint, payload);
+resp = caller.Response;
+
 end
