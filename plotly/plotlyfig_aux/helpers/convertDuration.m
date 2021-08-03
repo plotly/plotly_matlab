17c6471637d428a907a85b65146576c1f14878dd
Abderahmane BENALI <46283596+benaliabderrahmane@users.noreply.github.com>
2021-08-03 15:36:24 +0100

    changed if statements with switchs

--- plotly/plotlyfig_aux/helpers/convertDuration.m
+++ plotly/plotlyfig_aux/helpers/convertDuration.m
@@ -1,26 +1,24 @@
 function [converted,type] = convertDuration(duration)
-if (strcmpi(duration.Format,'s'))
-    converted = seconds(duration);
-    type = 'sec';
-
-elseif (strcmpi(duration.Format,'m'))
-    converted = minutes(duration);
-    type = 'min';
-
-elseif (strcmpi(duration.Format,'h'))
-    converted = hours(duration); 
-    type = 'hr';
-
-elseif (strcmpi(duration.Format,'d'))
-    converted = days(duration); 
-    type = 'days';
-
-elseif (strcmpi(duration.Format,'y'))
-    converted = years(duration); 
-    type = 'yrs';
-else
+switch (duration.Format)
+    case 's'
+        converted = seconds(duration);
+        type = 'sec';     
+    case 'm'
+        converted = minutes(duration);
+        type = 'min';
+    case 'h'
+        converted = hours(duration); 
+        type = 'hr';
+    case 'd'
+        converted = days(duration); 
+        type = 'days';    
+    case 'y'
+        converted = years(duration); 
+        type = 'yrs';   
+    otherwise
     %no convertion is applied
     converted = duration;
     type = '';
+    
 end
 end
