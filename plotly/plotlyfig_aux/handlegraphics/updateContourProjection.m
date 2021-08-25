045f0d1646892868d2e9f5c87dc1585850ee5c2c
Gilberto Galvis <gilbertogalvis2227@gmail.com>
2021-08-25 16:28:47 -0400

    improve aspect ratio and camera eye using surfc matlab function

--- plotly/plotlyfig_aux/handlegraphics/updateContourProjection.m
+++ plotly/plotlyfig_aux/handlegraphics/updateContourProjection.m
@@ -10,7 +10,7 @@ axIndex = obj.getAxisIndex(obj.State.Plot(contourIndex).AssociatedAxis);
 axis_data = get(obj.State.Plot(contourIndex).AssociatedAxis);
 
 %-PLOT DATA STRUCTURE- %
-contour_data = get(obj.State.Plot(contourIndex).Handle)
+contour_data = get(obj.State.Plot(contourIndex).Handle);
 
 %-CHECK FOR MULTIPLE AXES-%
 [xsource, ysource] = findSourceAxis(obj,axIndex);
@@ -40,68 +40,29 @@ obj.data{contourIndex}.name = contour_data.DisplayName;
 xdata = contour_data.XData;
 ydata = contour_data.YData;
 zdata = contour_data.ZData;
+    
+%-contour type-%
+obj.data{contourIndex}.type = 'surface';
 
-if isvector(zdata)
-    
-    %-contour type-%
-    obj.data{contourIndex}.type = 'contour';
-    
-    %-contour x data-%
-    if ~isvector(x)
-        obj.data{contourIndex}.xdata = xdata(1,:);
-    else
-        obj.data{contourIndex}.xdata = xdata;
-    end
+%-contour x and y data
+obj.data{contourIndex}.x = xdata;
+obj.data{contourIndex}.y = ydata;
 
-    %-contour y data-%
-    if ~isvector(y)
-        obj.data{contourIndex}.ydata = ydata';
-    else
-        obj.data{contourIndex}.ydata = ydata';
-    end
-    
-    %-contour z data-%
-    obj.data{contourIndex}.z = zdata;
-    
-else
-    
-    %-contour type-%
-    obj.data{contourIndex}.type = 'surface';
-    
-    %-contour x and y data
-%     [xmesh, ymesh] = meshgrid(xdata, ydata);
-    obj.data{contourIndex}.x = xdata;
-    obj.data{contourIndex}.y = ydata;
-    
-    %-contour z data-%
-    obj.data{contourIndex}.z = zdata;%-2*ones(size(zdata));
-    
-    %-setting for contour lines z-direction-%
-    obj.data{contourIndex}.contours.z.start = contour_data.LevelList(1);
-    obj.data{contourIndex}.contours.z.end = contour_data.LevelList(end);
-    obj.data{contourIndex}.contours.z.size = contour_data.LevelStep;
-    obj.data{contourIndex}.contours.z.show = true;
-    obj.data{contourIndex}.contours.z.usecolormap = true;
-    obj.data{contourIndex}.hidesurface = true;
-    obj.data{contourIndex}.surfacecolor = zdata;
-    
-    obj.data{contourIndex}.contours.z.project.x = true;
-    obj.data{contourIndex}.contours.z.project.y = true;
-    obj.data{contourIndex}.contours.z.project.z = true;
-    
-end
+%-contour z data-%
+obj.data{contourIndex}.z = zdata;%-2*ones(size(zdata));
 
-%-------------------------------------------------------------------------%
+%-setting for contour lines z-direction-%
+obj.data{contourIndex}.contours.z.start = contour_data.LevelList(1);
+obj.data{contourIndex}.contours.z.end = contour_data.LevelList(end);
+obj.data{contourIndex}.contours.z.size = contour_data.LevelStep;
+obj.data{contourIndex}.contours.z.show = true;
+obj.data{contourIndex}.contours.z.usecolormap = true;
+obj.data{contourIndex}.hidesurface = true;
+obj.data{contourIndex}.surfacecolor = zdata;
 
-%-contour x type-%
-
-obj.data{contourIndex}.xtype = 'array';
-
-%-------------------------------------------------------------------------%
-
-%-contour y type-%
-
-obj.data{contourIndex}.ytype = 'array';
+obj.data{contourIndex}.contours.z.project.x = true;
+obj.data{contourIndex}.contours.z.project.y = true;
+obj.data{contourIndex}.contours.z.project.z = true;
 
 %-------------------------------------------------------------------------%
 
@@ -113,21 +74,6 @@ obj.data{contourIndex}.visible = strcmp(contour_data.Visible,'on');
 
 %-contour showscale-%
 obj.data{contourIndex}.showscale = false;
-
-%-------------------------------------------------------------------------%
-
-%-zauto-%
-obj.data{contourIndex}.zauto = false;
-
-%-------------------------------------------------------------------------%
-
-%-zmin-%
-obj.data{contourIndex}.zmin = axis_data.CLim(1);
-
-%-------------------------------------------------------------------------%
-
-%-zmax-%
-obj.data{contourIndex}.zmax = axis_data.CLim(2);
 
 %-------------------------------------------------------------------------%
 
@@ -144,69 +90,53 @@ end
 %-contour reverse scale-%
 obj.data{contourIndex}.reversescale = false;
 
-%-------------------------------------------------------------------------%
+%---------------------------------------------------------------------%
 
-%-autocontour-%
-obj.data{contourIndex}.autocontour = false;
+%-aspect ratio-%
+ar = obj.PlotOptions.AspectRatio;
 
-%-------------------------------------------------------------------------%
+if ~isempty(ar)
+    if ischar(ar)
+        obj.layout.scene.aspectmode = ar;
+    elseif isvector(ar) && length(ar) == 3
+        xar = ar(1);
+        yar = ar(2);
+        zar = ar(3);
+    end
+else
 
-%-contour contours-%
-
-%-coloring-%
-switch contour_data.Fill
-    case 'off'
-        obj.data{contourIndex}.contours.coloring = 'lines';
-    case 'on'
-        obj.data{contourIndex}.contours.coloring = 'fill';
+    %-define as default-%
+    xar = max(xdata(:));
+    yar = max(ydata(:));
+    xyar = max([xar, yar]);
+    zar = 0.6*xyar;
 end
 
-%-start-%
-obj.data{contourIndex}.contours.start = contour_data.TextList(1);
+obj.layout.scene.aspectratio.x = xyar;
+obj.layout.scene.aspectratio.y = xyar;
+obj.layout.scene.aspectratio.z = zar;
 
-%-end-%
-obj.data{contourIndex}.contours.end = contour_data.TextList(end);
+%---------------------------------------------------------------------%
 
-%-step-%
-obj.data{contourIndex}.contours.size = diff(contour_data.TextList(1:2));
+%-camera eye-%
+ey = obj.PlotOptions.CameraEye;
 
-%-------------------------------------------------------------------------%
-
-if(~strcmp(contour_data.LineStyle,'none'))
-    
-    %-contour line colour-%
-    if isnumeric(contour_data.LineColor)
-        col = 255*contour_data.LineColor;
-        obj.data{contourIndex}.line.color = ['rgb(' num2str(col(1)) ',' num2str(col(2)) ',' num2str(col(3)) ')'];
-    else
-        obj.data{contourIndex}.line.color = 'rgba(0,0,0,0)';
+if ~isempty(ey)
+    if isvector(ey) && length(ey) == 3
+        obj.layout.scene.camera.eye.x = ey(1);
+        obj.layout.scene.camera.eye.y = ey(2);
+        obj.layout.scene.camera.eye.z = ey(3);
     end
-    
-    %-contour line width-%
-    obj.data{contourIndex}.line.width = contour_data.LineWidth;
-    
-    %-contour line dash-%
-    switch contour_data.LineStyle
-        case '-'
-            LineStyle = 'solid';
-        case '--'
-            LineStyle = 'dash';
-        case ':'
-            LineStyle = 'dot';
-        case '-.'
-            LineStyle = 'dashdot';
-    end
-    
-    obj.data{contourIndex}.line.dash = LineStyle;
-    
-    %-contour smoothing-%
-    obj.data{contourIndex}.line.smoothing = 0;
-    
 else
+
+    %-define as default-%
+    xey = - xyar; if xey>0 xfac = -0.2; else xfac = 0.2; end
+    yey = - xyar; if yey>0 yfac = -0.2; else yfac = 0.2; end
+    if zar>0 zfac = 0.2; else zfac = -0.2; end
     
-    %-contours showlines-%
-    obj.data{contourIndex}.contours.showlines = false;
-    
+    obj.layout.scene.camera.eye.x = xey + xfac*xey; 
+    obj.layout.scene.camera.eye.y = yey + yfac*yey;
+    obj.layout.scene.camera.eye.z = zar + zfac*zar;
 end
 
 %-------------------------------------------------------------------------%
