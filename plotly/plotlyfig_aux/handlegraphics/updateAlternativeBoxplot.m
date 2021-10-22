b82e0b6bacd1d49e248ab11ffdc439de6976db90
Gilberto Galvis <gilbertogalvis2227@gmail.com>
2021-10-22 19:55:59 -0400

    fix issue #424

---
+++ plotly/plotlyfig_aux/handlegraphics/updateAlternativeBoxplot.m
@@ -0,0 +1,114 @@
+function obj = updateAlternativeBoxplot(obj, dataIndex)
+
+	%-------------------------------------------------------------------------%
+
+	%-INITIALIZATIONS-%
+
+	axIndex = obj.getAxisIndex(obj.State.Plot(dataIndex).AssociatedAxis);
+	plotStructure = get(obj.State.Plot(dataIndex).Handle);
+	plotData = plotStructure.Children;
+
+	nTraces = length(plotData);
+	traceIndex = dataIndex;
+
+	%-------------------------------------------------------------------------%
+
+	%-update traces-%
+
+	for t = 1:nTraces
+	
+		if t ~= 1
+			obj.PlotOptions.nPlots = obj.PlotOptions.nPlots + 1;
+			traceIndex = obj.PlotOptions.nPlots;
+		end
+
+		updateBoxplotLine(obj, axIndex, plotData(t), traceIndex);
+	end
+
+	%-------------------------------------------------------------------------%
+end
+
+function updateBoxplotLine(obj, axIndex, plotData, traceIndex)
+
+	%-------------------------------------------------------------------------%
+
+	%-INITIALIZATIONS-%
+
+	%-get axis info-%
+	[xSource, ySource] = findSourceAxis(obj, axIndex);
+
+	%-get trade data-%
+	xData = plotData.XData;
+	yData = plotData.YData;
+
+	if isduration(xData) || isdatetime(xData), xData = datenum(xData); end
+    if isduration(yData) || isdatetime(yData), yData = datenum(yData); end
+
+    if length(xData) < 2, xData = ones(1,2)*xData; end
+    if length(yData) < 2, yData = ones(1,2)*yData; end
+
+    %-------------------------------------------------------------------------%
+
+    %-set trace-%
+    obj.data{traceIndex}.type = 'scatter';
+    obj.data{traceIndex}.mode = getScatterMode(plotData);
+    obj.data{traceIndex}.visible = strcmp(plotData.Visible,'on');
+    obj.data{traceIndex}.name = plotData.DisplayName;
+    obj.data{traceIndex}.xaxis = sprintf('x%d', xSource);
+    obj.data{traceIndex}.yaxis = sprintf('y%d', ySource);
+
+    %-------------------------------------------------------------------------%
+
+    %-set trace data-%
+    obj.data{traceIndex}.x = xData;
+    obj.data{traceIndex}.y = yData;
+
+    %-------------------------------------------------------------------------%
+
+    %-set marker properties-%
+    obj.data{traceIndex}.marker = extractLineMarker(plotData);
+
+    %-set line properties-%
+    obj.data{traceIndex}.line = extractLineLine(plotData);
+
+    %-------------------------------------------------------------------------%
+
+    %-legend-%
+    leg = get(plotData.Annotation);
+    legInfo = get(leg.LegendInformation);
+
+    switch legInfo.IconDisplayStyle
+        case 'on'
+            showLeg = true;
+        case 'off'
+            showLeg = false;
+    end
+
+    obj.data{traceIndex}.showlegend = showLeg;
+
+    if isempty(obj.data{traceIndex}.name)
+        obj.data{traceIndex}.showlegend = false;
+    end
+
+    %-------------------------------------------------------------------------%
+end
+
+function scatterMode = getScatterMode(plotData)
+
+	marker = plotData.Marker;
+	lineStyle = plotData.LineStyle;
+
+	if ~strcmpi('none', marker) && ~strcmpi('none', lineStyle)
+        scatterMode = 'lines+markers';
+
+    elseif ~strcmpi('none', marker)
+        scatterMode = 'markers';
+
+    elseif ~strcmpi('none', lineStyle)
+        scatterMode = 'lines';
+
+    else
+        scatterMode = 'none';
+
+    end
+end
