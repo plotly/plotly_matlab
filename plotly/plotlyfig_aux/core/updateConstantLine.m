9ceb9ae572e68bed752d4133da8240ca4c498cb6
Roberto Moura <robertoffmoura@gmail.com>
2025-03-17 20:05:19 +0000

    Add label to constant lines

--- plotly/plotlyfig_aux/core/updateConstantLine.m
+++ plotly/plotlyfig_aux/core/updateConstantLine.m
@@ -29,6 +29,70 @@ function updateConstantLine(obj,plotIndex)
         obj.data{plotIndex}.y = yaxis.range;
     end
 
+    if ~isempty(plotData.Label)
+        annotation = struct();
+
+        annotation.showarrow = false;
+
+        annotation.xref = "x"+xsource;
+        annotation.yref = "y"+ysource;
+
+        if plotData.InterceptAxis == "x"
+            annotation.textangle = -90;
+        end
+
+        annotation.xanchor = plotData.LabelHorizontalAlignment;
+
+        switch plotData.LabelVerticalAlignment
+            case {"top", "cap"}
+                annotation.yanchor = "top";
+            case "middle"
+                annotation.yanchor = "middle";
+            case {"baseline","bottom"}
+                annotation.yanchor = "bottom";
+        end
+
+        annotation.text = parseString( ...
+                plotData.Label, plotData.Interpreter);
+        annotation.text = "<b>" + join( ...
+                string(annotation.text), "<br>") + "</b>";
+
+        if plotData.InterceptAxis == "x"
+            annotation.x = plotData.Value;
+            annotation.y = yaxis.range(2);
+        else
+            annotation.x = xaxis.range(2);
+            annotation.y = plotData.Value;
+        end
+
+        col = round(255*plotData.LabelColor);
+        annotation.font.color = sprintf("rgb(%d,%d,%d)", col);
+
+        annotation.font.family = matlab2plotlyfont(plotData.FontName);
+        annotation.font.size = plotData.FontSize;
+        switch plotData.FontWeight
+            case {"bold","demi"}
+                annotation.text = "<b>" + annotation.text + "</b>";
+            otherwise
+        end
+
+        if plotData.LabelHorizontalAlignment == "center"
+            if plotData.InterceptAxis == "x"
+                ylim = plotData.Parent.YLim;
+                textWidth = text(0,0,plotData.Label,units="normalized",rotation=90,Visible="off").Extent(4);
+                textWidth = textWidth * (ylim(2) - ylim(1));
+                obj.data{plotIndex}.y(2) = obj.data{plotIndex}.y(2) - textWidth;
+            else
+                xlim = plotData.Parent.XLim;
+                textWidth = text(0,0,plotData.Label,units="normalized",Visible="off").Extent(3);
+                textWidth = textWidth * (xlim(2) - xlim(1));
+                obj.data{plotIndex}.x(2) = obj.data{plotIndex}.x(2) - textWidth;
+            end
+        end
+
+        obj.layout.annotations{end+1} = annotation;
+    end
+
     %---------------------------------------------------------------------%
 
     %-Fro 3D plots-%
