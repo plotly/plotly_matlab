7b73aa28fe6fc55817a62ae67d59351e2182feca
robertoffmoura <robertoffmoura@gmail.com>
2025-05-29 12:48:34 +0100

    Refactor extractPatchLine.m

--- plotly/plotlyfig_aux/helpers/extractPatchLine.m
+++ plotly/plotlyfig_aux/helpers/extractPatchLine.m
@@ -3,59 +3,52 @@ function line = extractPatchLine(patch_data)
     % OF TYPE "LINE". THESE OBJECTS ARE USED IN LINESERIES,
     % STAIRSERIES, STEMSERIES, BASELINESERIES, AND BOXPLOTS
 
-    %-AXIS STRUCTURE-%
-    axis_data = ancestor(patch_data.Parent,'axes');
-
-    %-FIGURE STRUCTURE-%
-    figure_data = ancestor(patch_data.Parent,'figure');
-
-    %-INITIALIZE OUTPUT-%
     line = struct();
+    if patch_data.LineStyle == "none"
+        return
+    end
 
-    %-PATCH LINE COLOR-%
+    cLim = ancestor(patch_data.Parent, "axes").CLim;
+    colormap = ancestor(patch_data.Parent, "figure").Colormap;
+    faceVertexCData = patch_data.FaceVertexCData(1,1);
+    cDataMapping = patch_data.CDataMapping;
 
-    colormap = figure_data.Colormap;
+    line.color = extractColor(patch_data.EdgeColor, cDataMapping, colormap, cLim, faceVertexCData);
+    line.width = patch_data.LineWidth;
+    line.dash = extractLineStyle(patch_data.LineStyle);
+end
 
-    if (~strcmp(patch_data.LineStyle,'none'))
-        if isnumeric(patch_data.EdgeColor)
-            col = round(255*patch_data.EdgeColor);
-            line.color = getStringColor(col);
-        else
-            switch patch_data.EdgeColor
-                case 'none'
-                    line.color = 'rgba(0,0,0,0)';
-                case 'flat'
-                    switch patch_data.CDataMapping
-                        case 'scaled'
-                            capCD = max(min( ...
-                                    patch_data.FaceVertexCData(1,1), ...
-                                    axis_data.CLim(2)), axis_data.CLim(1));
-                            scalefactor = (capCD - axis_data.CLim(1)) ...
-                                    / diff(axis_data.CLim);
-                            col = round(255*(colormap(1+floor(scalefactor ...
-                                    * (length(colormap)-1)),:)));
-                        case 'direct'
-                            col = round(255*(colormap( ...
-                                    patch_data.FaceVertexCData(1,1),:)));
-                    end
-                    line.color = getStringColor(col);
-            end
+function out = extractColor(color, cDataMapping, colormap, cLim, faceVertexCData)
+    if isnumeric(color)
+        out = getStringColor(round(255*color));
+    else
+        switch color
+            case "none"
+                out = "rgba(0,0,0,0)";
+            case "flat"
+                switch cDataMapping
+                    case "scaled"
+                        capCD = max(min(faceVertexCData, cLim(2)), cLim(1));
+                        scalefactor = (capCD - cLim(1)) / diff(cLim);
+                        col = colormap(1+floor(scalefactor ...
+                                * (length(colormap)-1)),:);
+                    case "direct"
+                        col = colormap(faceVertexCData,:);
+                end
+                out = getStringColor(round(255*col));
         end
+    end
+end
 
-        %-PATCH LINE WIDTH (STYLE)-%
-        line.width = patch_data.LineWidth;
-
-        %-PATCH LINE DASH (STYLE)-%
-        switch patch_data.LineStyle
-            case '-'
-                LineStyle = 'solid';
-            case '--'
-                LineStyle = 'dash';
-            case ':'
-                LineStyle = 'dot';
-            case '-.'
-                LineStyle = 'dashdot';
-        end
-        line.dash = LineStyle;
+function out = extractLineStyle(lineStyle)
+    switch lineStyle
+        case "-"
+            out = "solid";
+        case "--"
+            out = "dash";
+        case ":"
+            out = "dot";
+        case "-."
+            out = "dashdot";
     end
 end
