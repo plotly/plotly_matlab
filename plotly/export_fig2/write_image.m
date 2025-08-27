96e8fd9184dc35210adb2a9bac7311d083b3b6ee
robertoffmoura <robertoffmoura@gmail.com>
2025-08-27 11:54:36 +0100

    Refactor write_image

--- plotly/export_fig2/write_image.m
+++ plotly/export_fig2/write_image.m
@@ -1,32 +1,28 @@
-function output = write_image(pfObj, imageFormat, filename, height, width, scale)
+function output = write_image(pfObj, options)
     % Function to write plotly figures to a supported image format, which
     % are the following: "png", "jpg", "jpeg", "webp", "svg", "pdf", "eps",
     % "json".
-
-    debug=0;
-    if nargin < 2
-        imageFormat = "png";
-        filename = "figure.png";
-        height = pfObj.layout.height;
-        width = pfObj.layout.width;
-        scale = 1;
-    elseif nargin < 3
-        filename = "figure." + imageFormat;
-        height = pfObj.layout.height;
-        width = pfObj.layout.width;
-        scale = 1;
-    elseif nargin < 4
-        height = pfObj.layout.height;
-        width = pfObj.layout.width;
-        scale = 1;
-    elseif nargin < 5
-        width = pfObj.layout.width;
-        scale = 1;
-    elseif nargin < 6
-        scale = 1;
+    arguments
+        pfObj
+        options.imageFormat string = "png"
+        options.filename string = ""
+        options.height double = pfObj.layout.height
+        options.width double = pfObj.layout.width
+        options.scale double = 1
     end
 
-    if strcmpi(imageFormat, "jpg")
+    % Set default filename based on imageFormat if not provided
+    if options.filename == ""
+        options.filename = "figure." + options.imageFormat;
+    end
+
+    imageFormat = options.imageFormat;
+    filename = options.filename;
+    height = options.height;
+    width = options.width;
+    scale = options.scale;
+
+    if imageFormat == "jpg"
         imageFormat = "jpeg";
     end
 
@@ -49,28 +45,26 @@ function output = write_image(pfObj, imageFormat, filename, height, width, scale
 
     if ~isfile(kExec) || ~isfile(plyJsLoc)
         status = getKaleido();
-    else
-        status = 1;
-    end
-
-    if status == 0
-        return
+        if status == 0
+            return
+        end
     end
 
     mjLoc = replace(string(fullfile( ...
             wd, "kaleido", "etc", "mathjax", "MathJax.js")), "\", "/");
-    scope="plotly";
+    scope = "plotly";
 
     % Prepare input plotly object for Kaleido
-    q = struct();
-    q.data.data = pfObj.data;
-    q.data.layout = pfObj.layout;
-    q.data.layout = rmfield(q.data.layout, "height");
-    q.data.layout = rmfield(q.data.layout, "width");
-    q.format = string(imageFormat);
-    q.height = height;
-    q.scale = scale;
-    q.width = width;
+    q = struct( ...
+        "data", struct( ...
+            "data", pfObj.data, ...
+            "layout", rmfield(pfObj.layout, ["height" "width"]) ...
+        ), ...
+        "format", string(imageFormat), ...
+        "height", height, ...
+        "scale", scale, ...
+        "width", width ...
+    );
 
     pfJson = native2unicode(jsonencode(q), "UTF-8");
     tFile = string(fullfile(wd, "kaleido", "temp.txt"));
@@ -84,34 +78,27 @@ function output = write_image(pfObj, imageFormat, filename, height, width, scale
             + "--allow-file-access-from-files --disable-breakpad " ...
             + "--disable-dev-shm-usage"];
 
-    if debug
-        inputCmd = char(join(cmd, ""));
-        fprintf("\nDebug info:\n%s\n\n", inputCmd);
-    end
-
     [code,out] = system(char(join(cmd, "")));
-    if debug
-        disp(out);
-    end
 
     if code ~= 0
         fprintf("\nFatal: Failed to run Kaleido.\n\n");
-        return;
-    else
-        a = string(split(out,newline));
-        if a(end) == ""
-            a(end) = [];
-        end
-        output = jsondecode(a(end));
+        return
     end
+
+    a = string(split(out,newline));
+    if a(end) == ""
+        a(end) = [];
+    end
+    output = jsondecode(a(end));
 
     if output.code ~= 0
         fprintf("\nError: %s\n", output.message);
-    else
-        out = unicode2native(output.result, "UTF-8");
-        out = matlab.net.base64decode(out);
-        f = fopen(char(filename), "wb");
-        fwrite(f, out);
-        fclose(f);
+        return
     end
+
+    out = unicode2native(output.result, "UTF-8");
+    out = matlab.net.base64decode(out);
+    f = fopen(char(filename), "wb");
+    fwrite(f, out);
+    fclose(f);
 end
