6b95c4a9557a5d4ac3c35b6009c98d86305051a6
Roberto Moura <roberto@ffmoura.com>
2026-08-04 16:03:14 +0000

    histogramOrientation: exclude 3D bar patches with nonzero ZData

--- plotly/plotlyfig_aux/helpers/histogramOrientation.m
+++ plotly/plotlyfig_aux/helpers/histogramOrientation.m
@@ -5,6 +5,15 @@ function orientation = histogramOrientation(hist_data)
     try
         tmpXData = get(hist_data, 'XData');
         tmpYData = get(hist_data, 'YData');
+        % Octave's bar3/bar3h create 3D patches that also have four
+        % rows of data; a histogram patch is always flat in z
+        tmpZData = [];
+        if isprop(hist_data, 'ZData')
+            tmpZData = get(hist_data, 'ZData');
+        end
+        if ~isempty(tmpZData) && any(nonzeros(tmpZData))
+            return
+        end
         % check to see if patch is in the shape of "vertical" rectangles :)
         if size(get(hist_data, 'XData'),1) == 4 ...
                 && size(get(hist_data, 'XData'), 2) > 1 ...
