74cc516e11ac5ff3d24dbf5079324279aceb6897
Roberto Moura <roberto@ffmoura.com>
2026-08-05 13:03:28 +0100

    updateQuivergroup: skip invisible children and concatenate z data for quiver3

--- plotly/plotlyfig_aux/handlegraphics/updateQuivergroup.m
+++ plotly/plotlyfig_aux/handlegraphics/updateQuivergroup.m
@@ -7,20 +7,33 @@ function obj = updateQuivergroup(obj, quiverIndex)
 
     xdata = [];
     ydata = [];
-    %iterate through first two children (the vector line + arrow head)
-    for n = 1:2;
+    zdata = [];
+    %-process visible children (shafts + heads), skipping data-holder
+    %-children that have no line or marker style-%
+    for n = 1:numel(quiver_child)
+        qt = quiver_child(n);
+        if strcmp(get(qt, 'LineStyle'), 'none') ...
+                && strcmp(get(qt, 'Marker'), 'none')
+            continue
+        end
         %-update line -%
-        obj.State.Plot(quiverIndex).Handle = quiver_child(n);
+        obj.State.Plot(quiverIndex).Handle = qt;
         obj.data{quiverIndex} = updateLineseries(obj,quiverIndex);
 
         %update xdata and ydata
         xdata = [xdata obj.data{quiverIndex}.x];
         ydata = [ydata obj.data{quiverIndex}.y];
+        if isfield(obj.data{quiverIndex}, 'z')
+            zdata = [zdata obj.data{quiverIndex}.z];
+        end
     end
 
     % store the final data vector
     obj.data{quiverIndex}.x = xdata;
     obj.data{quiverIndex}.y = ydata;
+    if ~isempty(zdata)
+        obj.data{quiverIndex}.z = zdata;
+    end
 
     %-revert handle-%
     obj.State.Plot(quiverIndex).Handle = quiver_group;
