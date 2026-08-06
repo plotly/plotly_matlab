321a0b312ae7fe3bf54265b5a25ba151bd0a5445
Roberto Moura <roberto@ffmoura.com>
2026-08-06 12:42:42 +0000

    polar hovertext: use literal θ instead of unrecognised \u escape

--- plotly/plotlyfig_aux/handlegraphics/updateLineseries.m
+++ plotly/plotlyfig_aux/handlegraphics/updateLineseries.m
@@ -123,7 +123,7 @@ function data = updateLineseries(obj, plotIndex)
     if isCompassArrow
         data.r = rData(1:2);
         data.theta = thetaData(1:2);
-        data.hovertext = {'', sprintf("r: %.2f<br>\u03B8: %.2f", rData(2), thetaData(2))};
+        data.hovertext = {'', sprintf("r: %.2f<br>θ: %.2f", rData(2), thetaData(2))};
         data.hoverinfo = 'text';
     elseif isFeatherArrow
         data.x = xData(1:2);
@@ -174,7 +174,7 @@ function data = updateLineseries(obj, plotIndex)
                 end
             end
             if isPolar
-                data.hovertext = "r: " + data.r(:) + "<br>" + "\u03B8: " + ...
+                data.hovertext = "r: " + data.r(:) + "<br>" + "θ: " + ...
                         data.theta(:) + "<br>";
             elseif isPlot3D
                 data.hovertext = xDataLabel + ": " + data.x(:) + "<br>" ...
