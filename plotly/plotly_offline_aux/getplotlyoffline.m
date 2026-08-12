c643c62fe5febdefa0938b7cf3f6bdc4b38f389f
Roberto Moura <roberto@ffmoura.com>
2026-08-12 16:03:33 +0000

    getplotlyoffline: save bundle verbatim (escapechars corrupted the JS)

--- plotly/plotly_offline_aux/getplotlyoffline.m
+++ plotly/plotly_offline_aux/getplotlyoffline.m
@@ -19,7 +19,7 @@ function getplotlyoffline(plotly_bundle_url)
     validatedir(status, mess, messid, 'plotlyjs');
 
     % save bundle
-    bundle = escapechars(plotly_bundle);
+    bundle = plotly_bundle;
     bundle_name = 'plotly-matlab-offline-bundle.js';
     bundle_file = fullfile(plotly_js_folder, bundle_name);
     file_id = fopen(bundle_file, 'w');
